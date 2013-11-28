f1820ffc636cee5f3ebbb89a2db4850c898d0306
Daniel Godas-Lopez <dgodas@gmail.com>
2013-11-28 18:23:39 +0000

    added cygwin targets to the script and made cross-win* targets explicit

--- compile.sh
+++ compile.sh
@@ -3,11 +3,23 @@
 # Capstone Disassembler Engine
 # By Nguyen Anh Quynh <aquynh@gmail.com>, 2013>
 
+function build {
+	CROSS= make clean
+
+	if [ ${CC}x != x ]; then
+		make CC=$CC
+	else
+		make
+	fi
+}
+
 case "$1" in
-  "" ) make clean; make;;
-  "nix32" ) make clean; CFLAGS=-m32 LDFLAGS=-m32 make;;
-  "clang" ) make clean; make CC=clang;;
-  "win32" ) make clean; make CROSS=i686-w64-mingw32- windows;;
-  "win64" ) make clean; make CROSS=x86_64-w64-mingw32- windows;;
-  * ) echo "Usage: compile.sh [nix32|clang|win32|win64]"; exit 1;;
+  "" ) build;;
+  "nix32" ) CFLAGS=-m32 LDFLAGS=-m32 build;;
+  "clang" ) CC=clang build;;
+  "cross-win32" ) CROSS=i686-w64-mingw32- build;;
+  "cross-win64" ) CROSS=x86_64-w64-mingw32- build;;
+  "cygwin-mingw32" ) CROSS=i686-pc-mingw32- build;;
+  "cygwin-mingw64" ) CROSS=x86_64-w64-mingw32- build;;
+  * ) echo "Usage: compile.sh [nix32|clang|cross-win32|cross-win64|cygwin-mingw32|cygwin-mingw64]"; exit 1;;
 esac
