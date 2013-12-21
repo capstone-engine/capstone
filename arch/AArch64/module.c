13a7d95763a9c9481799ad9bda77415e68882b3d
Nguyen Anh Quynh <aquynh@gmail.com>
2013-12-21 12:19:44 +0800

    add missing arch/*/module.c

---
+++ arch/AArch64/module.c
@@ -0,0 +1,29 @@
+/* Capstone Disassembler Engine */
+/* By Dang Hoang Vu <danghvu@gmail.com> 2013 */
+
+#include "../../cs_priv.h"
+#include "../../MCRegisterInfo.h"
+#include "AArch64Disassembler.h"
+#include "AArch64InstPrinter.h"
+#include "mapping.h"
+
+
+static void init_arm64(cs_struct *ud)
+{
+	MCRegisterInfo *mri = malloc(sizeof(*mri));
+
+	AArch64_init(mri);
+	ud->printer = AArch64_printInst;
+	ud->printer_info = mri;
+	ud->getinsn_info = mri;
+	ud->disasm = AArch64_getInstruction;
+	ud->reg_name = AArch64_reg_name;
+	ud->insn_id = AArch64_get_insn_id;
+	ud->insn_name = AArch64_insn_name;
+	ud->post_printer = AArch64_post_printer;
+}
+
+static void __attribute__ ((constructor)) __init_arm64__()
+{
+	init_arch[CS_ARCH_ARM64] = init_arm64;
+}
