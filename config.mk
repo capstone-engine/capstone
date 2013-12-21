00f01f1d4eadfdadf5667d2c9b20a98847494e79
Nguyen Anh Quynh <aquynh@gmail.com>
2013-12-22 00:49:16 +0800

    add comments for config.mk

--- config.mk
+++ config.mk
@@ -1,7 +1,16 @@
-# comment out what you don’t need
+# NOTE: only enable one of the lines below by commenting out what you want.
+# That is either one of 4 architectures, or *all* of them.
 
-CAPSTONE_ARCH = arm
-CAPSTONE_ARCH = arm64
-CAPSTONE_ARCH = mips
-CAPSTONE_ARCH = x86
+# comment out line below if you only want ARM support
+#CAPSTONE_ARCH = arm
+
+# comment out line below if you only want ARM64 support
+#CAPSTONE_ARCH = arm64
+
+# comment out line below if you only want Mips support
+#CAPSTONE_ARCH = mips
+
+# comment out line below if you only want X86 support
+#CAPSTONE_ARCH = x86
+
 CAPSTONE_ARCH = all   # <— by default we support all archs
