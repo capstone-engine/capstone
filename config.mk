5219f64f845e2b109886f0f3429481c69803fc0f
Nguyen Anh Quynh <aquynh@gmail.com>
2013-12-22 13:00:48 +0800

    make it less error-prone to handle config.mk

--- config.mk
+++ config.mk
@@ -1,2 +1,14 @@
-CAPSTONE_ARCHS = arm x86 mips aarch64
+# Specify which archs you want to compile in
+CAPSTONE_ARCHS =
 
+# Comment the line below if you don't want to ARM support
+CAPSTONE_ARCHS += arm
+
+# Comment the line below if you don't want to ARM64 support
+CAPSTONE_ARCHS += aarch64
+
+# Comment the line below if you don't want to Mips support
+CAPSTONE_ARCHS += mips
+
+# Comment the line below if you don't want to X86 support
+CAPSTONE_ARCHS += x86
