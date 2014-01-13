f48a879e314679617f4ebd881f2372985585925f
Nguyen Anh Quynh <aquynh@gmail.com>
2014-01-13 16:25:36 +0800

    suite: benchmark.py can benchmark specific archs, rather than all archs like before

--- suite/benchmark.py
+++ suite/benchmark.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
 # Simple benchmark for Capstone by disassembling random code. By Nguyen Anh Quynh, 2014
+# Syntax:
+# ./suite/benchmark.py          --> Benchmark all archs
+# ./suite/benchmark.py x86      --> Benchmark all X86 (all 16bit, 32bit, 64bit)
+# ./suite/benchmark.py x86-32   --> Benchmark X86-32 arch only
+# ./suite/benchmark.py arm      --> Benchmark all ARM (arm, thumb)
+# ./suite/benchmark.py aarch64  --> Benchmark ARM-64
+# ./suite/benchmark.py mips     --> Benchmark all Mips (32bit, 64bit)
+# ./suite/benchmark.py ppc      --> Benchmark PPC
 
 from capstone import *
 
 from time import time
 from random import randint
+import sys
 
 
 # file providing code to disassemble
@@ -13,15 +22,15 @@ FILE = '/usr/bin/python'
 
 
 all_tests = (
-        (CS_ARCH_X86, CS_MODE_16, "X86 16bit (Intel syntax)", 0),
-        (CS_ARCH_X86, CS_MODE_32, "X86 32bit (ATT syntax)", CS_OPT_SYNTAX_ATT),
-        (CS_ARCH_X86, CS_MODE_32, "X86 32 (Intel syntax)", 0),
-        (CS_ARCH_X86, CS_MODE_64, "X86 64 (Intel syntax)", 0),
+        (CS_ARCH_X86, CS_MODE_16, "X86-16bit (Intel syntax)", 0),
+        (CS_ARCH_X86, CS_MODE_32, "X86-32bit (ATT syntax)", CS_OPT_SYNTAX_ATT),
+        (CS_ARCH_X86, CS_MODE_32, "X86-32 (Intel syntax)", 0),
+        (CS_ARCH_X86, CS_MODE_64, "X86-64 (Intel syntax)", 0),
         (CS_ARCH_ARM, CS_MODE_ARM, "ARM", 0),
-        (CS_ARCH_ARM, CS_MODE_THUMB, "THUMB-2", 0),
+        (CS_ARCH_ARM, CS_MODE_THUMB, "THUMB (ARM)", 0),
         (CS_ARCH_MIPS, CS_MODE_32 + CS_MODE_BIG_ENDIAN, "MIPS-32 (Big-endian)", 0),
         (CS_ARCH_MIPS, CS_MODE_64 + CS_MODE_LITTLE_ENDIAN, "MIPS-64-EL (Little-endian)", 0),
-        (CS_ARCH_ARM64, CS_MODE_ARM, "ARM-64", 0),
+        (CS_ARCH_ARM64, CS_MODE_ARM, "ARM-64 (AArch64)", 0),
         (CS_ARCH_PPC, CS_MODE_BIG_ENDIAN, "PPC-64", 0),
         (CS_ARCH_PPC, CS_MODE_BIG_ENDIAN, "PPC-64, print register with number only", CS_OPT_SYNTAX_NOREGNAME),
         )
@@ -52,6 +61,13 @@ md.detail = False
 cfile = open(FILE)
 
 for (arch, mode, comment, syntax) in all_tests:
+    try:
+        request = sys.argv[1]
+        if not request in comment.lower():
+            continue
+    except:
+        pass
+
     print("Platform: %s" %comment)
 
     try:
