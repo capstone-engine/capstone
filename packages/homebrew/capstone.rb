a118137634eb14f562c22663f0f7e53b87022af5
Nguyen Anh Quynh <aquynh@gmail.com>
2014-03-05 11:53:27 +0800

    packages: another attempt for Brew formula

--- packages/homebrew/capstone.rb
+++ packages/homebrew/capstone.rb
@@ -5,6 +5,11 @@ class Capstone < Formula
   url 'http://capstone-engine.org/download/2.1/capstone-2.1.tgz'
   sha1 '3e5fe91684cfc76d73caa857a268332ac9d40659'
 
+  def patches
+    # fix pkgconfig path
+    "https://github.com/aquynh/capstone/blob/next/packages/homebrew/patch-Makefile-2.1"
+  end
+
   def install
     inreplace 'Makefile', 'lib64', 'lib'
     system "./make.sh"
