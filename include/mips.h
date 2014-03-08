395bbab2dec67be942e396ec756209aa498cdb3d
Nguyen Anh Quynh <aquynh@gmail.com>
2014-03-09 04:08:11 +0800

    cleanup x86.h & mips.h (C-IMM)

--- include/mips.h
+++ include/mips.h
@@ -35,8 +35,8 @@ typedef struct cs_mips_op {
 	mips_op_type type;	// operand type
 	union {
 		unsigned int reg;	// register value for REG operand
-		int64_t imm;		// immediate value for C-IMM or IMM operand
-		mips_op_mem mem;		// base/index/scale/disp value for MEM operand
+		int64_t imm;		// immediate value for IMM operand
+		mips_op_mem mem;	// base/index/scale/disp value for MEM operand
 	};
 } cs_mips_op;
 
