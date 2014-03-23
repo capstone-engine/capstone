61aaabbba0198424c685ee5acc83107c0c3c394e
Nguyen Anh Quynh <aquynh@gmail.com>
2014-03-23 22:56:38 +0800

    suite: add SystemZ support to benchmark.py & fuzz.py

--- suite/fuzz.py
+++ suite/fuzz.py
@@ -40,6 +40,7 @@ all_tests = (
         (CS_ARCH_PPC, CS_MODE_BIG_ENDIAN, "PPC", 0),
         (CS_ARCH_PPC, CS_MODE_BIG_ENDIAN, "PPC, print register with number only", CS_OPT_SYNTAX_NOREGNAME),
         (CS_ARCH_SPARC, CS_MODE_BIG_ENDIAN, "Sparc", 0),
+        (CS_ARCH_SYSZ, 0, "SystemZ", 0),
         )
 
 
