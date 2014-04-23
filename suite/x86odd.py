d71106047d8158011d2cf5130994fda67eb6de84
Nguyen Anh Quynh <aquynh@gmail.com>
2014-04-23 12:40:58 +0800

    suite: add some new instructions to x86odd.py

--- suite/x86odd.py
+++ suite/x86odd.py
@@ -6,10 +6,17 @@ import sys
 from capstone import *
 
 CODE32  = b"\xc0\xe0\x02"
-CODE32 += b"\xc0\xf0\x02"
-CODE32 += b"\xc1\xf6\x00"
+CODE32 += b"\xc0\xf6\x02"              # sal dh, 0
+CODE32 += b"\xc1\xf6\x00"              # sal esi, 0
 CODE32 += b"\x82\xc0\x00"
-CODE32 += b"\x0f\x1a\x00"
+CODE32 += b"\x0f\x1a\x00"              # nop dword ptr [eax]
+CODE32 += b"\xf7\xc0\x11\x22\x33\x44"  # test eax, 0x44332211
+CODE32 += b"\xf7\xc8\x11\x22\x33\x44"  # test eax, 0x44332211
+CODE32 += b"\xf7\x88\x00\x00\x00\x00\x00\x00\x00\x00"  # test dword ptr [eax], 0
+CODE32 += b"\xf6\x88\x00\x00\x00\x00\x00"              # test byte ptr [eax], 0
+
+CODE32 += b"\xd9\xd8"
+CODE32 += b"\xdf\xdf"
 
 _python3 = sys.version_info.major == 3
 
