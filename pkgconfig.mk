bf29a467f8338803a795d871128762dc269d26ed
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-04-27 23:34:49 +0200

    Replace build system with autotools
    This is needed by Frida in order to do out-of-tree builds for cross-
    compilation with a single source tree. It also comes with the added
    bonus of `configure` switches and `make dist` for building releases.

--- pkgconfig.mk
+++
@@ -1,9 +0,0 @@
-# Package version of Capstone for Makefile.
-# To be used to generate capstone.pc for pkg-config
-
-# version major & minor 
-PKG_MAJOR = 2
-PKG_MINOR = 2
-
-# version bugfix level. Example: PKG_EXTRA = 1
-PKG_EXTRA =
