b2a88df20f57d318a1f005698d3d7acc9d046b0d
Nguyen Anh Quynh <aquynh@gmail.com>
2014-05-14 14:28:40 +0800

    MSVC added some code picked up by MingW, so cross-win32/74 compile is broken. this fixes the issue

--- arch/Mips/MipsMapping.c
+++ arch/Mips/MipsMapping.c
@@ -8,8 +8,10 @@
 
 #include "../../include/mips.h"
 #include "../../utils.h"
+#if !defined(__MINGW32__) && !defined(__MINGW64__)
 #if defined (WIN32) || defined (WIN64) || defined (_WIN32) || defined (_WIN64)
 #include "platform.h"
+#endif
 #endif
 #include "MipsMapping.h"
 
