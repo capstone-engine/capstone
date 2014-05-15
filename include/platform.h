e079f10a8063e9bb3fe9419d817ae0f844306e66
Nguyen Anh Quynh <aquynh@gmail.com>
2014-05-15 14:34:16 +0800

    remove more redundant code in platform.h

--- include/platform.h
+++ include/platform.h
@@ -8,23 +8,6 @@
 #if !defined(__MINGW32__) && !defined(__MINGW64__)  // this is not MingW
 #if defined (WIN32) || defined (WIN64) || defined (_WIN32) || defined (_WIN64)
 
-// inttypes.h
-typedef signed char  int8_t;
-typedef signed short int16_t;
-typedef signed int   int32_t;
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
-typedef signed long long   int64_t;
-typedef unsigned long long uint64_t;
-
-// stdbool.h
-#ifndef __cplusplus
-//    typedef unsigned char bool;
-#define false 0
-#define true 1
-#endif
-
 
 // string.h
 #define strcasecmp _stricmp
