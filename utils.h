2b68355a671d66de52b23eec5df11db87f545920
Nguyen Anh Quynh <aquynh@gmail.com>
2014-05-28 00:01:30 +0800

    msvc: support strcpy() with strcpy_s()

--- utils.h
+++ utils.h
@@ -7,12 +7,13 @@
 #include "include/capstone.h"
 #include "cs_priv.h"
 
-// strcasecmp for MSVC
+// strcasecmp() & strcpy() for MSVC
 #if !defined(__MINGW32__) && !defined(__MINGW64__)  // this is not MingW
 #if defined (WIN32) || defined (WIN64) || defined (_WIN32) || defined (_WIN64)
 
 // string.h
 #define strcasecmp _stricmp
+#define strcpy strcpy_s
 
 #endif	// MSVC
 #endif	// not MingW
