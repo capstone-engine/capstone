d0023197eb633b39e1908cdc9f8785f14f9ea45d
Nguyen Anh Quynh <aquynh@gmail.com>
2014-05-28 15:15:00 +0800

    cmake: support Sparc, SystemZ & XCore

--- arch/XCore/XCoreInstPrinter.h
+++ arch/XCore/XCoreInstPrinter.h
@@ -13,6 +13,6 @@ void XCore_printInst(MCInst *MI, SStream *O, void *Info);
 void XCore_post_printer(csh ud, cs_insn *insn, char *insn_asm, MCInst *mci);
 
 // extract details from assembly code @code
-void XCore_insn_extract(MCInst *MI, char *code);
+void XCore_insn_extract(MCInst *MI, const char *code);
 
 #endif
