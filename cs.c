6ddd715facf15ac386c8eaf2e52fa66f00a5ddc7
Nguyen Anh Quynh <aquynh@gmail.com>
2014-06-08 19:11:38 +0800

    we have to consider \t in input buffer of fill_insn()

--- cs.c
+++ cs.c
@@ -305,24 +305,18 @@ static void fill_insn(struct cs_struct *handle, cs_insn *insn, char *buffer, MCI
 		}
 	} else {
 		for (sp = buffer; *sp; sp++) {
-			//if (*sp == ' '||*sp == '\t')
-			if (*sp == ' ')
+			if (*sp == ' '||*sp == '\t')
 				break;
 		}
 	}
 
 	if (*sp) {
 		*sp = '\0';
-		//strcpy(insn->op_str, sp+1);
-		strncpy(insn->op_str, sp+1, sizeof(insn->op_str) - 1);
-		insn->op_str[sizeof(insn->op_str) - 1] = '\0';
-		/*
 		// find the next non-space char
 		sp++;
 		for (; ((*sp == ' ') || (*sp == '\t')); sp++);
 		strncpy(insn->op_str, sp, sizeof(insn->op_str) - 1);
 		insn->op_str[sizeof(insn->op_str) - 1] = '\0';
-		*/
 	} else
 		insn->op_str[0] = '\0';
 
