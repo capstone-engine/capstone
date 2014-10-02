0c6c6222b24acb51287248d2afcbf3d7c85e4f9a
Félix Cloutier <felix@felixcloutier.com>
2014-10-02 16:21:38 -0400

    Readded targets because I screwed up

---
+++ xcode/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Félix on 2014-10-02.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
