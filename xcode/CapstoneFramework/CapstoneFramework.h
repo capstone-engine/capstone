25414e301fc20f9c0da00b27a6ea0bf971f4dd4e
Félix Cloutier <felix@felixcloutier.com>
2014-10-03 02:12:39 -0400

    Adding framework target

---
+++ xcode/CapstoneFramework/CapstoneFramework.h
@@ -0,0 +1,19 @@
+//
+//  CapstoneFramework.h
+//  CapstoneFramework
+//
+//  Created by Félix on 2014-10-03.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CapstoneFramework.
+FOUNDATION_EXPORT double CapstoneFrameworkVersionNumber;
+
+//! Project version string for CapstoneFramework.
+FOUNDATION_EXPORT const unsigned char CapstoneFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CapstoneFramework/PublicHeader.h>
+
+
