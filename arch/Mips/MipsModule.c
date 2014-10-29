1084f3afdaf5eefe1a80ff54e96278662afc8586
Nguyen Anh Quynh <aquynh@gmail.com>
2014-10-29 22:20:38 +0800

    mips: properly support modes MIPS32R6 & CS_MODE_MIPSGP64

--- arch/Mips/MipsModule.c
+++ arch/Mips/MipsModule.c
@@ -15,7 +15,8 @@ static cs_err init(cs_struct *ud)
 
 	// verify if requested mode is valid
 	if (ud->mode & ~(CS_MODE_LITTLE_ENDIAN | CS_MODE_32 | CS_MODE_64 |
-				CS_MODE_MICRO | CS_MODE_BIG_ENDIAN))
+				CS_MODE_MICRO | CS_MODE_MIPS32R6 |
+				CS_MODE_MIPSGP64 | CS_MODE_BIG_ENDIAN))
 		return CS_ERR_MODE;
 
 	mri = cs_mem_malloc(sizeof(*mri));
