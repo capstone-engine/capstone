3b964d2c0acb1001641c74e7b34bdc2c3d34613c
Nguyen Anh Quynh <aquynh@gmail.com>
2015-01-28 13:39:31 +0800

    python: build the core with make.sh for all non-Windows system

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -107,7 +107,7 @@ class custom_build_clib(build_clib):
             os.chdir("src")
 
             # platform description refers at https://docs.python.org/2/library/sys.html#sys.platform
-            if SYSTEM == "linux2" or SYSTEM == "darwin":
+            if SYSTEM != "win32":
                 os.chmod("make.sh", stat.S_IREAD|stat.S_IEXEC)
                 os.system("BUILD_CORE_ONLY=yes ./make.sh")
 
