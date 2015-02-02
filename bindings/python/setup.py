bc3ffd4c642a6a9117d23d0ba50980a0f8c5713d
Nguyen Tan Cong <shenlongbk@gmail.com>
2015-02-02 22:32:46 +0700

    copy msvc/headers/ in both linux and win32 system

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -35,9 +35,7 @@ def copy_sources():
 
     dir_util.copy_tree("../../arch", "src/arch/")
     dir_util.copy_tree("../../include", "src/include/")
-
-    if SYSTEM == "win32":
-        dir_util.copy_tree("../../msvc/headers", "src/msvc/headers")
+    dir_util.copy_tree("../../msvc/headers", "src/msvc/headers")
 
     src.extend(glob.glob("../../*.[ch]"))
     src.extend(glob.glob("../../*.mk"))
