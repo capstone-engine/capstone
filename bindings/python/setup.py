42e8388b61527d9caf25930ab11c809225c1395a
Nguyen Anh Quynh <aquynh@gmail.com>
2015-02-03 20:18:05 +0800

    python: do not include msvcr120.dll because we can static compile it

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -15,9 +15,7 @@ from distutils.sysconfig import get_python_lib
 
 # prebuilt libraries for Windows - for sdist
 PATH_LIB64 = "prebuilt/win64/capstone.dll"
-PATH_MSVCR64 = "prebuilt/win64/msvcr120.dll"
 PATH_LIB32 = "prebuilt/win32/capstone.dll"
-PATH_MSVCR32 = "prebuilt/win32/msvcr120.dll"
 
 # package name can be 'capstone' or 'capstone_win'
 PKG_NAME = 'capstone'
@@ -104,11 +102,9 @@ class custom_build_clib(build_clib):
             # if Windows prebuilt library is available, then include it
             if is_64bits and os.path.exists(PATH_LIB64):
                 SETUP_DATA_FILES.append(PATH_LIB64)
-                SETUP_DATA_FILES.append(PATH_MSVCR64)
                 return
             elif os.path.exists(PATH_LIB32):
                 SETUP_DATA_FILES.append(PATH_LIB32)
-                SETUP_DATA_FILES.append(PATH_MSVCR32)
                 return
 
         # build library from source if src/ is existent
