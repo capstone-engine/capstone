19ee2d10b3b4a8ff4ce1f7d00c52b3c3b37c0356
Cr4sh <cr4sh0@gmail.com>
2015-03-29 18:29:06 +0800

    inttypes.h fix

--- arch/ARM/ARMDisassembler.c
+++ arch/ARM/ARMDisassembler.c
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "../../inttypes.h"
+#include "../../myinttypes.h"
 
 #include "ARMAddressingModes.h"
 #include "ARMBaseInfo.h"
