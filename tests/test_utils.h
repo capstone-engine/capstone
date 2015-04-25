26ba3f3540ba1ea2adb9130880fe65c078fd6840
learn_more <learn_more@unknowncheats.me>
2015-04-25 02:02:32 +0200

    Headers should have a header guard.

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -1,3 +1,5 @@
+#ifndef CAPSTONE_TEST_UTILS_H
+#define CAPSTONE_TEST_UTILS_H
 
 void print_string_hex(char *comment, unsigned char *str, size_t len);
 
@@ -9,3 +11,5 @@ struct group_name {
 void test_groups_common(csh handle, int* error, struct group_name* groups, size_t count);
 
 #define COUNTOF(x)		(sizeof(x) / sizeof(x[0]))
+
+#endif // CAPSTONE_TEST_UTILS_H
