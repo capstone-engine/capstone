01a744c329b37de68e55c1df46953e4cc5fb3bfc
derrek <derrek.haxx@yahoo.com>
2015-05-08 13:08:55 +0200

    arm: Added instruction group ARM_GRP_INT, ARM_INS_SVC is now using it.

--- include/capstone/arm.h
+++ include/capstone/arm.h
@@ -888,6 +888,7 @@ typedef enum arm_insn_group {
 	ARM_GRP_JUMP,	// = CS_GRP_JUMP
 	ARM_GRP_CALL,	// = CS_GRP_CALL
 	ARM_GRP_PRIVILEGE = 6, // = CS_GRP_PRIVILEGE
+	ARM_GRP_INT,	// = CS_GRP_INT
 
 	//> Architecture-specific groups
 	ARM_GRP_CRYPTO = 128,
