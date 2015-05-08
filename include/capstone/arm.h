c85a0e4f44c3f119cc6ed3b4d3e975e35f9c4b76
Nguyen Anh Quynh <aquynh@gmail.com>
2015-05-08 23:54:37 +0800

    arm: correct value of ARM_GRP_INT (=CS_GRP_INT=4)

--- include/capstone/arm.h
+++ include/capstone/arm.h
@@ -887,8 +887,8 @@ typedef enum arm_insn_group {
 	// all jump instructions (conditional+direct+indirect jumps)
 	ARM_GRP_JUMP,	// = CS_GRP_JUMP
 	ARM_GRP_CALL,	// = CS_GRP_CALL
+	ARM_GRP_INT = 4, // = CS_GRP_INT
 	ARM_GRP_PRIVILEGE = 6, // = CS_GRP_PRIVILEGE
-	ARM_GRP_INT,	// = CS_GRP_INT
 
 	//> Architecture-specific groups
 	ARM_GRP_CRYPTO = 128,
