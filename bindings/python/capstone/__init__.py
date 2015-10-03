a496bbfc0cead0c4fabdca183bad4f5bdf847eb5
Nicolas PLANEL <nplanel@gmail.com>
2015-10-03 21:11:17 +1000

    [binding][python] fixup python 3 compatibility
    In Python 3.x dict.keys() return a dict_keys object not a list.

--- bindings/python/capstone/__init__.py
+++ bindings/python/capstone/__init__.py
@@ -875,8 +875,7 @@ def debug():
 
     all_archs = ""
     keys = archs.keys()
-    keys.sort()
-    for k in keys:
+    for k in sorted(keys):
         if cs_support(archs[k]):
             all_archs += "-%s" % k
 
