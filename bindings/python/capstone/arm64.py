8c6968f202dfa4eea9a0bcf05d6ab116d3d833cb
Nguyen Anh Quynh <aquynh@gmail.com>
2016-04-12 13:42:07 +0800

    python: add access field for ARM64

--- bindings/python/capstone/arm64.py
+++ bindings/python/capstone/arm64.py
@@ -38,6 +38,7 @@ class Arm64Op(ctypes.Structure):
         ('ext', ctypes.c_uint),
         ('type', ctypes.c_uint),
         ('value', Arm64OpValue),
+        ('access', ctypes.c_uint8),
     )
 
     @property
