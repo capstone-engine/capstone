10730fa971544a02a8061d25650c5fe2c61db639
Nguyen Anh Quynh <aquynh@gmail.com>
2016-04-24 10:02:56 +0800

    x86: do not print 'ptr' for fxsave & fxrstor instructions. this fixes issue #492

--- arch/X86/X86IntelInstPrinter.c
+++ arch/X86/X86IntelInstPrinter.c
@@ -64,6 +64,7 @@ static void set_mem_access(MCInst *MI, bool status)
 static void printopaquemem(MCInst *MI, unsigned OpNo, SStream *O)
 {
 	// FIXME: do this with autogen
+	// printf(">>> ID = %u\n", MI->flat_insn->id);
 	switch(MI->flat_insn->id) {
 		default:
 			SStream_concat0(O, "ptr ");
@@ -72,6 +73,8 @@ static void printopaquemem(MCInst *MI, unsigned OpNo, SStream *O)
 		case X86_INS_SIDT:
 		case X86_INS_LGDT:
 		case X86_INS_LIDT:
+		case X86_INS_FXRSTOR:
+		case X86_INS_FXSAVE:
 			// do not print "ptr"
 			break;
 	}
