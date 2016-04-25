882052a8ae764552b219c743120950d5bc373da2
tandasat <tanda.sat@gmail.com>
2016-04-24 20:04:22 -0700

    change copyright

--- windows/winkernel_mm.c
+++ windows/winkernel_mm.c
@@ -1,5 +1,5 @@
 /* Capstone Disassembly Engine */
-/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2016 */
+/* By Satoshi Tanda <tanda.sat@gmail.com>, 2016 */
 #include "winkernel_mm.h"
 #include <ntddk.h>
 
