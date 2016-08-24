e6d61c8b3c69c3acad52b7e1fddf431d28f571fd
Jelle Vergeer <vergeer.jelle@gmail.com>
2016-08-24 16:02:14 +0200

    - Fix fox changed structure member size

--- bindings/python/capstone/x86.py
+++ bindings/python/capstone/x86.py
@@ -64,7 +64,7 @@ class CsX86(ctypes.Structure):
         ('addr_size', ctypes.c_uint8),
         ('modrm', ctypes.c_uint8),
         ('sib', ctypes.c_uint8),
-        ('disp', ctypes.c_int32),
+        ('disp', ctypes.c_int64),
         ('sib_index', ctypes.c_uint),
         ('sib_scale', ctypes.c_int8),
         ('sib_base', ctypes.c_uint),
