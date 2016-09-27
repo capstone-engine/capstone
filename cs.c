d7e38cd903ac74994ab9b963340b85945b2a9d53
Satoshi Tanda <tanda.sat@gmail.com>
2016-09-27 08:08:58 -0700

    suppress MSVC code analysis (PREfast) warnings
    Signed-off-by: Satoshi Tanda <tanda.sat@gmail.com>

--- cs.c
+++ cs.c
@@ -1,7 +1,8 @@
 /* Capstone Disassembly Engine */
 /* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2014 */
 #if defined (WIN32) || defined (WIN64) || defined (_WIN32) || defined (_WIN64)
-#pragma warning(disable:4996)
+#pragma warning(disable:4996)			// disable MSVC's warning on strcpy()
+#pragma warning(disable:28719)		// disable MSVC's warning on strcpy()
 #endif
 #if defined(CAPSTONE_HAS_OSXKERNEL)
 #include <libkern/libkern.h>
