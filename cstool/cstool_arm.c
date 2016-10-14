08da0c053368b00c61728aad54b395c1d6a4e6ef
YUHANG TANG <yuhangtang@YUHANGs-MacBook-Pro.local>
2016-10-14 20:47:29 +0800

    cstool : print all debug information for all arch.

---
+++ cstool/cstool_arm.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <platform.h>
+#include <capstone.h>
+
+
+static void print_string_hex(char *comment, unsigned char *str, size_t len)
+{
+    unsigned char *c;
+    
+    printf("%s", comment);
+    for (c = str; c < str + len; c++) {
+        printf("0x%02x ", *c & 0xff);
+    }
+    
+    printf("\n");
+}
+
+void print_insn_detail_arm(csh handle, cs_insn *ins)
+{
+    cs_arm *arm;
+    int i;
+    
+    // detail can be NULL on "data" instruction if SKIPDATA option is turned ON
+    if (ins->detail == NULL)
+        return;
+    
+    arm = &(ins->detail->arm);
+    
+    if (arm->op_count)
+        printf("\top_count: %u\n", arm->op_count);
+    
+    for (i = 0; i < arm->op_count; i++) {
+        cs_arm_op *op = &(arm->operands[i]);
+        switch((int)op->type) {
+            default:
+                break;
+            case ARM_OP_REG:
+                printf("\t\toperands[%u].type: REG = %s\n", i, cs_reg_name(handle, op->reg));
+                break;
+            case ARM_OP_IMM:
+                printf("\t\toperands[%u].type: IMM = 0x%x\n", i, op->imm);
+                break;
+            case ARM_OP_FP:
+#if defined(_KERNEL_MODE)
+                // Issue #681: Windows kernel does not support formatting float point
+                printf("\t\toperands[%u].type: FP = <float_point_unsupported>\n", i);
+#else
+                printf("\t\toperands[%u].type: FP = %f\n", i, op->fp);
+#endif
+                break;
+            case ARM_OP_MEM:
+                printf("\t\toperands[%u].type: MEM\n", i);
+                if (op->mem.base != X86_REG_INVALID)
+                    printf("\t\t\toperands[%u].mem.base: REG = %s\n",
+                           i, cs_reg_name(handle, op->mem.base));
+                if (op->mem.index != X86_REG_INVALID)
+                    printf("\t\t\toperands[%u].mem.index: REG = %s\n",
+                           i, cs_reg_name(handle, op->mem.index));
+                if (op->mem.scale != 1)
+                    printf("\t\t\toperands[%u].mem.scale: %u\n", i, op->mem.scale);
+                if (op->mem.disp != 0)
+                    printf("\t\t\toperands[%u].mem.disp: 0x%x\n", i, op->mem.disp);
+                
+                break;
+            case ARM_OP_PIMM:
+                printf("\t\toperands[%u].type: P-IMM = %u\n", i, op->imm);
+                break;
+            case ARM_OP_CIMM:
+                printf("\t\toperands[%u].type: C-IMM = %u\n", i, op->imm);
+                break;
+            case ARM_OP_SETEND:
+                printf("\t\toperands[%u].type: SETEND = %s\n", i, op->setend == ARM_SETEND_BE? "be" : "le");
+                break;
+            case ARM_OP_SYSREG:
+                printf("\t\toperands[%u].type: SYSREG = %u\n", i, op->reg);
+                break;
+        }
+        
+        if (op->shift.type != ARM_SFT_INVALID && op->shift.value) {
+            if (op->shift.type < ARM_SFT_ASR_REG)
+                // shift with constant value
+                printf("\t\t\tShift: %u = %u\n", op->shift.type, op->shift.value);
+            else
+                // shift with register
+                printf("\t\t\tShift: %u = %s\n", op->shift.type,
+                       cs_reg_name(handle, op->shift.value));
+        }
+        
+        if (op->vector_index != -1) {
+            printf("\t\toperands[%u].vector_index = %u\n", i, op->vector_index);
+        }
+        
+        if (op->subtracted)
+            printf("\t\tSubtracted: True\n");
+    }
+    
+    if (arm->cc != ARM_CC_AL && arm->cc != ARM_CC_INVALID)
+        printf("\tCode condition: %u\n", arm->cc);
+    
+    if (arm->update_flags)
+        printf("\tUpdate-flags: True\n");
+    
+    if (arm->writeback)
+        printf("\tWrite-back: True\n");
+    
+    if (arm->cps_mode)
+        printf("\tCPSI-mode: %u\n", arm->cps_mode);
+    
+    if (arm->cps_flag)
+        printf("\tCPSI-flag: %u\n", arm->cps_flag);
+    
+    if (arm->vector_data)
+        printf("\tVector-data: %u\n", arm->vector_data);
+    
+    if (arm->vector_size)
+        printf("\tVector-size: %u\n", arm->vector_size);
+    
+    if (arm->usermode)
+        printf("\tUser-mode: True\n");
+    
+    if (arm->mem_barrier)
+        printf("\tMemory-barrier: %u\n", arm->mem_barrier);
+    
+    printf("\n");
+}
