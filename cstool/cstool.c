1f88b198f20bbb0f7e3b26ef763aaa0ec66bc842
Nguyen Anh Quynh <aquynh@gmail.com>
2016-10-21 17:34:31 +0800

    cstool: print out core version

--- cstool/cstool.c
+++ cstool/cstool.c
@@ -73,7 +73,7 @@ static uint8_t *preprocess(char *code, size_t *size)
 
 static void usage(char *prog)
 {
-	printf("Cstool v%s for Capstone Disassembler Engine (www.capstone-engine.org)\n\n", VERSION);
+	printf("Cstool v%s for Capstone Disassembler Engine (core v%u.%u)\n\n", VERSION, CS_API_MAJOR, CS_API_MINOR);
 	printf("Syntax: %s [-d:print all detail information] <arch+mode> <assembly-hexstring> [start-address-in-hex-format]\n", prog);
 	printf("\nThe following <arch+mode> options are supported:\n");
 
