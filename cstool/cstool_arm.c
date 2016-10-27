5e0a15521bf9947693661518ec687f1bd1866192
YUHANG TANG <yuhangtang@YUHANGs-MacBook-Pro.local>
2016-10-27 12:12:59 +0800

    cstool: add m68k and change cstool_x86

--- cstool/cstool_arm.c
+++ cstool/cstool_arm.c
@@ -9,16 +9,18 @@ void print_insn_detail_arm(csh handle, cs_insn *ins)
 {
 	cs_arm *arm;
 	int i;
-
+	cs_regs regs_read, regs_write;
+	uint8_t regs_read_count, regs_write_count;
+	
 	// detail can be NULL on "data" instruction if SKIPDATA option is turned ON
 	if (ins->detail == NULL)
 		return;
-
+	
 	arm = &(ins->detail->arm);
-
+	
 	if (arm->op_count)
 		printf("\top_count: %u\n", arm->op_count);
-
+	
 	for (i = 0; i < arm->op_count; i++) {
 		cs_arm_op *op = &(arm->operands[i]);
 		switch((int)op->type) {
@@ -40,17 +42,19 @@ void print_insn_detail_arm(csh handle, cs_insn *ins)
 				break;
 			case ARM_OP_MEM:
 				printf("\t\toperands[%u].type: MEM\n", i);
-				if (op->mem.base != X86_REG_INVALID)
+				if (op->mem.base != ARM_REG_INVALID)
 					printf("\t\t\toperands[%u].mem.base: REG = %s\n",
-							i, cs_reg_name(handle, op->mem.base));
-				if (op->mem.index != X86_REG_INVALID)
+						   i, cs_reg_name(handle, op->mem.base));
+				if (op->mem.index != ARM_REG_INVALID)
 					printf("\t\t\toperands[%u].mem.index: REG = %s\n",
-							i, cs_reg_name(handle, op->mem.index));
+						   i, cs_reg_name(handle, op->mem.index));
 				if (op->mem.scale != 1)
 					printf("\t\t\toperands[%u].mem.scale: %u\n", i, op->mem.scale);
 				if (op->mem.disp != 0)
 					printf("\t\t\toperands[%u].mem.disp: 0x%x\n", i, op->mem.disp);
-
+				if (op->mem.lshift != 0)
+					printf("\t\t\toperands[%u].mem.lshift: 0x%x\n", i, op->mem.lshift);
+				
 				break;
 			case ARM_OP_PIMM:
 				printf("\t\toperands[%u].type: P-IMM = %u\n", i, op->imm);
@@ -65,7 +69,25 @@ void print_insn_detail_arm(csh handle, cs_insn *ins)
 				printf("\t\toperands[%u].type: SYSREG = %u\n", i, op->reg);
 				break;
 		}
-
+		
+		if (op->neon_lane != -1) {
+			printf("\t\toperands[%u].neon_lane = %u\n", i, op->neon_lane);
+		}
+		
+		switch(op->access) {
+			default:
+				break;
+			case CS_AC_READ:
+				printf("\t\toperands[%u].access: READ\n", i);
+				break;
+			case CS_AC_WRITE:
+				printf("\t\toperands[%u].access: WRITE\n", i);
+				break;
+			case CS_AC_READ | CS_AC_WRITE:
+				printf("\t\toperands[%u].access: READ | WRITE\n", i);
+				break;
+		}
+		
 		if (op->shift.type != ARM_SFT_INVALID && op->shift.value) {
 			if (op->shift.type < ARM_SFT_ASR_REG)
 				// shift with constant value
@@ -73,43 +95,64 @@ void print_insn_detail_arm(csh handle, cs_insn *ins)
 			else
 				// shift with register
 				printf("\t\t\tShift: %u = %s\n", op->shift.type,
-						cs_reg_name(handle, op->shift.value));
+					   cs_reg_name(handle, op->shift.value));
 		}
-
+		
 		if (op->vector_index != -1) {
 			printf("\t\toperands[%u].vector_index = %u\n", i, op->vector_index);
 		}
-
+		
 		if (op->subtracted)
 			printf("\t\tSubtracted: True\n");
 	}
-
+	
 	if (arm->cc != ARM_CC_AL && arm->cc != ARM_CC_INVALID)
 		printf("\tCode condition: %u\n", arm->cc);
-
+	
 	if (arm->update_flags)
 		printf("\tUpdate-flags: True\n");
-
+	
 	if (arm->writeback)
 		printf("\tWrite-back: True\n");
-
+	
 	if (arm->cps_mode)
 		printf("\tCPSI-mode: %u\n", arm->cps_mode);
-
+	
 	if (arm->cps_flag)
 		printf("\tCPSI-flag: %u\n", arm->cps_flag);
-
+	
 	if (arm->vector_data)
 		printf("\tVector-data: %u\n", arm->vector_data);
-
+	
 	if (arm->vector_size)
 		printf("\tVector-size: %u\n", arm->vector_size);
-
+	
 	if (arm->usermode)
 		printf("\tUser-mode: True\n");
-
+	
 	if (arm->mem_barrier)
 		printf("\tMemory-barrier: %u\n", arm->mem_barrier);
-
+	
+	// Print out all registers accessed by this instruction (either implicit or explicit)
+	if (!cs_regs_access(handle, ins,
+						regs_read, &regs_read_count,
+						regs_write, &regs_write_count)) {
+		if (regs_read_count) {
+			printf("\tRegisters read:");
+			for(i = 0; i < regs_read_count; i++) {
+				printf(" %s", cs_reg_name(handle, regs_read[i]));
+			}
+			printf("\n");
+		}
+		
+		if (regs_write_count) {
+			printf("\tRegisters modified:");
+			for(i = 0; i < regs_write_count; i++) {
+				printf(" %s", cs_reg_name(handle, regs_write[i]));
+			}
+			printf("\n");
+		}
+	}
+	
 	printf("\n");
 }
