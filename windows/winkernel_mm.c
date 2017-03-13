4858174124b0469736f276ab7630d45a68ad5d0a
quangnh89 <quangnh89@gmail.com>
2017-03-13 21:54:02 +0700

    fix some style issues.

--- windows/winkernel_mm.c
+++ windows/winkernel_mm.c
@@ -34,15 +34,16 @@ void * CAPSTONE_API cs_winkernel_malloc(size_t size)
 
 	// FP; a use of NonPagedPool is required for Windows 7 support
 #pragma prefast(suppress : 30030)		// Allocating executable POOL_TYPE memory
-	size_t numberOfBytes = 0;
+	size_t number_of_bytes = 0;
 	CS_WINKERNEL_MEMBLOCK *block = NULL;
 	// A specially crafted size value can trigger the overflow.
 	// If the sum in a value that overflows or underflows the capacity of the type,
 	// the function returns NULL.
-	if (!NT_SUCCESS(RtlSizeTAdd(size, sizeof(CS_WINKERNEL_MEMBLOCK), &numberOfBytes)))
+	if (!NT_SUCCESS(RtlSizeTAdd(size, sizeof(CS_WINKERNEL_MEMBLOCK), &number_of_bytes))) {
 		return NULL;
+	}
 	block = (CS_WINKERNEL_MEMBLOCK *)ExAllocatePoolWithTag(
-			NonPagedPool, numberOfBytes, CS_WINKERNEL_POOL_TAG);
+			NonPagedPool, number_of_bytes, CS_WINKERNEL_POOL_TAG);
 	if (!block) {
 		return NULL;
 	}
