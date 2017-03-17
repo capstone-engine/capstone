5be7444c09e291e9ea0f4c11ba6118115945f947
Nguyen Anh Quynh <aquynh@gmail.com>
2017-03-17 23:44:34 +0800

    x86: support BND prefix. issue #872

--- arch/X86/X86Mapping.c
+++ arch/X86/X86Mapping.c
@@ -47609,6 +47609,52 @@ static bool valid_repne(cs_struct *h, unsigned int opcode)
 	return false;
 }
 
+// given MCInst's id, find out if this insn is valid for BND prefix
+// BND prefix is valid for CALL/JMP/RET
+static bool valid_bnd(cs_struct *h, unsigned int opcode)
+{
+	unsigned int id;
+	int i = insn_find(insns, ARR_SIZE(insns), opcode, &h->insn_cache);
+	if (i != 0) {
+		id = insns[i].mapid;
+		switch(id) {
+			default:
+				return false;
+
+			case X86_INS_JAE:
+            case X86_INS_JA:
+            case X86_INS_JBE:
+            case X86_INS_JB:
+            case X86_INS_JCXZ:
+            case X86_INS_JECXZ:
+            case X86_INS_JE:
+            case X86_INS_JGE:
+            case X86_INS_JG:
+            case X86_INS_JLE:
+            case X86_INS_JL:
+            case X86_INS_JMP:
+            case X86_INS_JNE:
+            case X86_INS_JNO:
+            case X86_INS_JNP:
+            case X86_INS_JNS:
+            case X86_INS_JO:
+            case X86_INS_JP:
+            case X86_INS_JRCXZ:
+            case X86_INS_JS:
+
+            case X86_INS_CALL:
+            case X86_INS_RET:
+            case X86_INS_RETF:
+            case X86_INS_RETFQ:
+
+				return true;
+		}
+	}
+
+	// not found
+	return false;
+}
+
 // given MCInst's id, find out if this insn is valid for REP prefix
 static bool valid_rep(cs_struct *h, unsigned int opcode)
 {
@@ -47745,6 +47791,8 @@ bool X86_lockrep(MCInst *MI, SStream *O)
 			if (valid_repne(MI->csh, opcode)) {
 				SStream_concat(O, "repne|");
 				add_cx(MI);
+			} else if (valid_bnd(MI->csh, opcode)) {
+				SStream_concat(O, "bnd|");
 			} else {
 				// invalid prefix
 				MI->x86_prefix[0] = 0;
