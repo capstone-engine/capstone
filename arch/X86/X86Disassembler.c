e510114011cf868f7a659765c31392eff5189027
Philippe Antoine <philippe.antoine@loria.fr>
2017-04-21 14:54:40 +0200

    Not resetting opcode to 0 in this case as well

--- arch/X86/X86Disassembler.c
+++ arch/X86/X86Disassembler.c
@@ -230,7 +230,9 @@ static void translateImmediate(MCInst *mcInst, uint64_t immediate,
 				case X86_CMPSSrr:  NewOpc = X86_CMPSSrr_alt;  break;
 			}
 			// Switch opcode to the one that doesn't get special printing.
-			MCInst_setOpcode(mcInst, NewOpc);
+            if (NewOpc != 0) {
+                MCInst_setOpcode(mcInst, NewOpc);
+            }
 		}
 #endif
 	} else if (type == TYPE_IMM5) {
