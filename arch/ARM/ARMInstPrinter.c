079d94d7f081a93c3465325819e0d6e030b9882a
Nguyen Anh Quynh <aquynh@gmail.com>
2017-05-04 17:20:01 +0800

    arm: POP {reg} read/write SP register. this fixes #913

--- arch/ARM/ARMInstPrinter.c
+++ arch/ARM/ARMInstPrinter.c
@@ -588,6 +588,11 @@ void ARM_printInst(MCInst *MI, SStream *O, void *Info)
 									MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].type = ARM_OP_REG;
 									MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].reg = MCOperand_getReg(MCInst_getOperand(MI, 0));
 									MI->flat_insn->detail->arm.op_count++;
+                                    // this instruction implicitly read/write SP register
+                                    MI->flat_insn->detail->regs_read[MI->flat_insn->detail->regs_read_count] = ARM_REG_SP;
+                                    MI->flat_insn->detail->regs_read_count++;
+                                    MI->flat_insn->detail->regs_write[MI->flat_insn->detail->regs_write_count] = ARM_REG_SP;
+                                    MI->flat_insn->detail->regs_write_count++;
 								}
 								SStream_concat0(O, "}");
 								return;
