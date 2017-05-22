44c419a64c8fd01be5d491d2a1f389fcd742beab
Nguyen Anh Quynh <aquynh@gmail.com>
2017-05-22 21:36:04 +0800

    x86: indentation

--- arch/X86/X86Mapping.c
+++ arch/X86/X86Mapping.c
@@ -47622,31 +47622,30 @@ static bool valid_bnd(cs_struct *h, unsigned int opcode)
 				return false;
 
 			case X86_INS_JAE:
-            case X86_INS_JA:
-            case X86_INS_JBE:
-            case X86_INS_JB:
-            case X86_INS_JCXZ:
-            case X86_INS_JECXZ:
-            case X86_INS_JE:
-            case X86_INS_JGE:
-            case X86_INS_JG:
-            case X86_INS_JLE:
-            case X86_INS_JL:
-            case X86_INS_JMP:
-            case X86_INS_JNE:
-            case X86_INS_JNO:
-            case X86_INS_JNP:
-            case X86_INS_JNS:
-            case X86_INS_JO:
-            case X86_INS_JP:
-            case X86_INS_JRCXZ:
-            case X86_INS_JS:
+			case X86_INS_JA:
+			case X86_INS_JBE:
+			case X86_INS_JB:
+			case X86_INS_JCXZ:
+			case X86_INS_JECXZ:
+			case X86_INS_JE:
+			case X86_INS_JGE:
+			case X86_INS_JG:
+			case X86_INS_JLE:
+			case X86_INS_JL:
+			case X86_INS_JMP:
+			case X86_INS_JNE:
+			case X86_INS_JNO:
+			case X86_INS_JNP:
+			case X86_INS_JNS:
+			case X86_INS_JO:
+			case X86_INS_JP:
+			case X86_INS_JRCXZ:
+			case X86_INS_JS:
 
-            case X86_INS_CALL:
-            case X86_INS_RET:
-            case X86_INS_RETF:
-            case X86_INS_RETFQ:
-
+			case X86_INS_CALL:
+			case X86_INS_RET:
+			case X86_INS_RETF:
+			case X86_INS_RETFQ:
 				return true;
 		}
 	}
