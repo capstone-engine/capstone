e12e2de2cf9b0f5529ad91e9e4f1f13398d07f1a
Nguyen Anh Quynh <aquynh@gmail.com>
2017-07-26 23:16:00 +0800

    cstool: cs_op_count() can return -1. fix #978

--- cstool/cstool_x86.c
+++ cstool/cstool_x86.c
@@ -235,7 +235,7 @@ void print_insn_detail_x86(csh ud, cs_mode mode, cs_insn *ins)
 
 	// Print out all immediate operands
 	count = cs_op_count(ud, ins, X86_OP_IMM);
-	if (count) {
+	if (count > 0) {
 		printf("\timm_count: %u\n", count);
 		for (i = 1; i < count + 1; i++) {
 			int index = cs_op_index(ud, ins, X86_OP_IMM, i);
