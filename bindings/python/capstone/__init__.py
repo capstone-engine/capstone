2106ed7c523b9c6b51c978d527ab9ad42704aa89
Erick Bauman <erick.bauman@utdallas.edu>
2017-08-23 21:01:41 -0500

    Added encoding field to instructions, as per encoding branch
    The encoding branch appears to have added some useful fields
    accessible from the public API, including the size and offsets
    of displacements and immediates in instructions.  I needed access
    to these fields, but the encoding branch is months behind the
    active branches, so I took the minimum code from the old encoding
    branch and put them into a more recent version of master.
    
    It does seem that the most recent version does not have an offset
    for the modRM byte in the InternalInstruction struct, so I did
    not keep this field when bringing it to the more recent version.
    
    I also added some of the changes made by user jellever, who added
    support for accessing these new fields from the python bindings.
    
    (cherry picked from commit d358c4b987cc77af90e24da15937e021c42f682f)

--- bindings/python/capstone/__init__.py
+++ bindings/python/capstone/__init__.py
@@ -554,7 +554,7 @@ class CsInsn(object):
             (self.prefix, self.opcode, self.rex, self.addr_size, \
                 self.modrm, self.sib, self.disp, \
                 self.sib_index, self.sib_scale, self.sib_base, self.sse_cc, \
-                self.avx_cc, self.avx_sae, self.avx_rm, self.operands) = x86.get_arch_info(self._detail.arch.x86)
+                self.avx_cc, self.avx_sae, self.avx_rm, self.operands, self.encoding) = x86.get_arch_info(self._detail.arch.x86)
         elif arch == CS_ARCH_MIPS:
                 self.operands = mips.get_arch_info(self._detail.arch.mips)
         elif arch == CS_ARCH_PPC:
