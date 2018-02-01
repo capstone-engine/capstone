3015e48c7042f6dbbf5c99f5f18349636a095187
Mazzeo Aldo <Mazzeo.A@ikarus.at>
2018-02-01 10:14:09 +0100

    Forgotten arrow led to compilation error

--- arch/X86/X86Disassembler.c
+++ arch/X86/X86Disassembler.c
@@ -851,7 +851,7 @@ bool X86_getInstruction(csh ud, const uint8_t *code, size_t code_len,
 					return false;
 			}
 
-			instr->imm_size = insn.immSize;
+			instr->imm_size = insn->immSize;
 			if (handle->detail) {
 				update_pub_insn(instr->flat_insn, insn, instr->x86_prefix);
 			} else {
