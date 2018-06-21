853a2870ce07a30d7ef259d4296a175843167891
Travis Finkenauer <tmfink@users.noreply.github.com>
2018-06-21 02:52:35 -0400

    Declare global arch arrays with contents (#1171)
    This eliminates the need for archs_enable() and eliminates the racey
    initialization.
    
    This makes the architecture-specific init, option, and destroy functions
    non-static so that they may be called from a different file.

--- arch/Sparc/SparcModule.c
+++ arch/Sparc/SparcModule.c
@@ -8,8 +8,9 @@
 #include "SparcDisassembler.h"
 #include "SparcInstPrinter.h"
 #include "SparcMapping.h"
+#include "SparcModule.h"
 
-static cs_err init(cs_struct *ud)
+cs_err Sparc_global_init(cs_struct *ud)
 {
 	MCRegisterInfo *mri;
 	mri = cs_mem_malloc(sizeof(*mri));
@@ -29,7 +30,7 @@ static cs_err init(cs_struct *ud)
 	return CS_ERR_OK;
 }
 
-static cs_err option(cs_struct *handle, cs_opt_type type, size_t value)
+cs_err Sparc_option(cs_struct *handle, cs_opt_type type, size_t value)
 {
 	if (type == CS_OPT_SYNTAX)
 		handle->syntax = (int) value;
@@ -41,20 +42,8 @@ static cs_err option(cs_struct *handle, cs_opt_type type, size_t value)
 	return CS_ERR_OK;
 }
 
-static void destroy(cs_struct *handle)
+void Sparc_destroy(cs_struct *handle)
 {
-}
-
-void Sparc_enable(void)
-{
-	cs_arch_init[CS_ARCH_SPARC] = init;
-	cs_arch_option[CS_ARCH_SPARC] = option;
-	cs_arch_destroy[CS_ARCH_SPARC] = destroy;
-	cs_arch_disallowed_mode_mask[CS_ARCH_SPARC] =
-		~(CS_MODE_BIG_ENDIAN | CS_MODE_V9);
-
-	// support this arch
-	all_arch |= (1 << CS_ARCH_SPARC);
 }
 
 #endif
