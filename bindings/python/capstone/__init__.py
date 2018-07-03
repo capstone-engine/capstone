25cb884722c9c0ab7578dfbe5bc49d494e85cce6
Steve Eckels <stevemk14ebr@gmail.com>
2018-07-02 23:29:21 -0400

    fixed py binding, again

--- bindings/python/capstone/__init__.py
+++ bindings/python/capstone/__init__.py
@@ -631,9 +631,9 @@ class CsInsn(object):
             (self.prefix, self.opcode, self.rex, self.addr_size, \
                 self.modrm, self.sib, self.disp, \
                 self.sib_index, self.sib_scale, self.sib_base, self.xop_cc, self.sse_cc, \
-                self.avx_cc, self.avx_sae, self.avx_rm, self.modrm_offset, \
-                self.disp_offset, self.disp_size, self.imm_offset, \
-                self.imm_size, self.operands) = x86.get_arch_info(self._detail.arch.x86)
+                self.avx_cc, self.avx_sae, self.avx_rm, self.eflags, \
+                self.modrm_offset, self.disp_offset, self.disp_size, self.imm_offset, self.imm_size, \
+                self.operands) = x86.get_arch_info(self._raw.detail.contents.arch.x86)
         elif arch == CS_ARCH_M68K:
                 (self.operands, self.op_size) = m68k.get_arch_info(self._raw.detail.contents.arch.m68k)
         elif arch == CS_ARCH_MIPS:
--- bindings/python/capstone/x86.py
+++ bindings/python/capstone/x86.py
@@ -1,7 +1,7 @@
 # Capstone Python bindings, by Nguyen Anh Quynnh <aquynh@gmail.com>
 
 import ctypes
-from . import copy_ctypes, copy_ctypes_list
+from . import copy_ctypes_list
 from .x86_const import *
 
 # define the API
@@ -43,6 +43,7 @@ class X86Op(ctypes.Structure):
     def mem(self):
         return self.value.mem
 
+
 class CsX86Encoding(ctypes.Structure):
     _fields_ = (
         ('modrm_offset', ctypes.c_uint8),
@@ -60,7 +61,7 @@ class CsX86(ctypes.Structure):
         ('addr_size', ctypes.c_uint8),
         ('modrm', ctypes.c_uint8),
         ('sib', ctypes.c_uint8),
-        ('disp', ctypes.c_int32),
+        ('disp', ctypes.c_int64),
         ('sib_index', ctypes.c_uint),
         ('sib_scale', ctypes.c_int8),
         ('sib_base', ctypes.c_uint),
@@ -79,7 +80,6 @@ def get_arch_info(a):
     return (a.prefix[:], a.opcode[:], a.rex, a.addr_size, \
             a.modrm, a.sib, a.disp, a.sib_index, a.sib_scale, \
             a.sib_base, a.xop_cc, a.sse_cc, a.avx_cc, a.avx_sae, a.avx_rm, a.eflags, \
-            a.encoding.modrm_offset, a.encoding.disp_offset, a.encoding.disp_size, \
-            a.encoding.imm_offset, a.encoding.imm_size, \
+            a.encoding.modrm_offset, a.encoding.disp_offset, a.encoding.disp_size, a.encoding.imm_offset, a.encoding.imm_size, \
             copy_ctypes_list(a.operands[:a.op_count]))
 
