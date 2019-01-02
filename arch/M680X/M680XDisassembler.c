59d7387d370da514c7db650c35f25a736c5a2673
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2019-01-03 00:48:55 +0100

    M680X: Fix clang-analyzer issue #1329. (#1334)

--- arch/M680X/M680XDisassembler.c
+++ arch/M680X/M680XDisassembler.c
@@ -650,14 +650,14 @@ static bool is_indexed09_post_byte_valid(const m680x_info *info,
 	case 0x9D: // [n16,PCR]
 		insn_description->insn_size += 2;
 		retval = read_byte(info, &ir, *address + 1);
-		address += 2;
+		*address += 2;
 		return retval;
 
 	case 0x9F: // [n]
 		insn_description->insn_size += 2;
 		retval = (post_byte & 0x60) == 0 &&
 			read_byte(info, &ir, *address + 1);
-		address += 2;
+		*address += 2;
 		return retval;
 	}
 
