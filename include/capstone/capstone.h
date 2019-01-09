9d36e93d411c215f260e1f9d1ea5fb3c0b7ae7b2
Nguyen Anh Quynh <aquynh@gmail.com>
2019-01-09 13:54:06 +0800

    next version = 5

--- include/capstone/capstone.h
+++ include/capstone/capstone.h
@@ -53,7 +53,7 @@ extern "C" {
 // Version for bleeding edge code of the Github's "next" branch.
 // Use this if you want the absolutely latest development code.
 // This version number will be bumped up whenever we have a new major change.
-#define CS_NEXT_VERSION 4
+#define CS_NEXT_VERSION 5
 
 // Capstone package version
 #define CS_VERSION_MAJOR CS_API_MAJOR
