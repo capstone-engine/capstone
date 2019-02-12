b8155ed58e4b3880640f01dea66a1aac74cdf1fa
hardtobelieve <tuanit96@gmail.com>
2019-02-12 02:02:12 -0800

    fixing capstone_test.c

--- suite/cstest/include/helper.h
+++ suite/cstest/include/helper.h
@@ -7,13 +7,14 @@
 #include <stdarg.h>
 #include <ctype.h>
 #include <dirent.h>
+#include "capstone_test.h"
 
 char **split(char *str, char *delim, int *size);
 void print_strs(char **list_str, int size);
 void free_strs(char **list_str, int size);
 void add_str(char **src, const char *format, ...);
-void trim_str(char **str);
-void replace_hex(char **src);
+void trim_str(char src[]);
+void replace_hex(char src[]);
 const char *get_filename_ext(const char *filename);
 
 char *readfile(const char *filename);
