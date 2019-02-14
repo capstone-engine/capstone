d9213efd6d82c29fd99cc33beb015920c15b7222
Nguyen Anh Quynh <aquynh@gmail.com>
2019-02-14 21:09:50 +0800

    cstest: fix build_cstest.sh

--- suite/cstest/build_cstest.sh
+++ suite/cstest/build_cstest.sh
@@ -2,8 +2,8 @@
 
 cd cmocka && mkdir build && cd build
 if [ "$UNAME" = Darwin ]; then
-cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make -j2 && sudo make install
-else  # Linux
 cmake .. && make -j2 && sudo make install
+else  # Linux
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make -j2 && sudo make install
 fi
 cd ../.. && make
