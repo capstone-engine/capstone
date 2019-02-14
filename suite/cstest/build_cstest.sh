7c6d044d86e91f0634c98e6f3e4a86f06634501b
Nguyen Anh Quynh <aquynh@gmail.com>
2019-02-14 21:17:35 +0800

    cstest: install cmocka into /usr/local for MacOS

--- suite/cstest/build_cstest.sh
+++ suite/cstest/build_cstest.sh
@@ -2,7 +2,7 @@
 
 cd cmocka && mkdir build && cd build
 if [ "$UNAME" = Darwin ]; then
-cmake .. && make -j2 && sudo make install
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j2 && sudo make install
 else  # Linux
 cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make -j2 && sudo make install
 fi
