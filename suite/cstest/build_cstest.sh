a24b5999a2b565d69ac5fe41d8fdd7bfbf7d7402
Nguyen Anh Quynh <aquynh@gmail.com>
2019-02-14 21:30:39 +0800

    cstest: fix install path for MacOS

--- suite/cstest/build_cstest.sh
+++ suite/cstest/build_cstest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd cmocka && mkdir build && cd build
-if [ "$UNAME" = Darwin ]; then
+if [ "$(uname)" = Darwin ]; then
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j2 && sudo make install
 else  # Linux
 cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make -j2 && sudo make install
