4adab3d72a88214cffbb6a6ffced366863cba86d
Nguyen Anh Quynh <aquynh@gmail.com>
2019-02-21 15:32:00 +0800

    X86: X86_insn_reg_att uses a wrong mapping array of Intel syntax

--- arch/X86/X86Mapping.c
+++ arch/X86/X86Mapping.c
@@ -3068,8 +3068,8 @@ x86_reg X86_insn_reg_att(unsigned int id, enum cs_ac_type *access)
 
 	for (i = 0; i < ARR_SIZE(insn_regs_att); i++) {
 		if (insn_regs_att[i].insn == id) {
-            if (access)
-                *access = insn_regs_intel[i].access;
+			if (access)
+				*access = insn_regs_att[i].access;
 			return insn_regs_att[i].reg;
 		}
 	}
