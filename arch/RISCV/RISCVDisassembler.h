1ee2a79cc20635711856c8262255ca05290a4464
fanfuqiang <feqin1023@gmail.com>
2019-02-28 21:47:39 +0800

    fix declaration conflict

--- arch/RISCV/RISCVDisassembler.h
+++ arch/RISCV/RISCVDisassembler.h
@@ -11,9 +11,8 @@
 
 void RISCV_init(MCRegisterInfo *MRI);
 
-bool RISCV_getInstruction(csh handle, const uint8_t * code, size_t code_len,
-			  MCInst * instr, uint16_t * size, uint64_t address,
-			  void *info);
-
+bool RISCV_getInstruction(csh ud, const uint8_t *code, size_t code_len,
+		          MCInst *instr, uint64_t *size, uint64_t address,
+		          void *info);
 
 #endif
