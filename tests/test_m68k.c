31d0de45527465578d27ef5d3189bd0dbba62f69
Travis Finkenauer <tmfink@users.noreply.github.com>
2019-03-02 04:40:29 -0500

    [M68K] store correct register value in op.reg_pair (#1411)
    * m68k: store correct m68k_reg value in op.reg_pair
    
    Originally, value - M68K_REG_D0 was stored and the print logic added
    M68K_REG_D0.
    
    * m68k: fix license typo

--- tests/test_m68k.c
+++ tests/test_m68k.c
@@ -137,7 +137,12 @@ static void print_insn_detail(cs_insn *ins)
 				break;
 			case M68K_OP_REG_BITS:
 				printf("\t\toperands[%u].type: REG_BITS = $%x\n", i, op->register_bits);
-
+				break;
+			case M68K_OP_REG_PAIR:
+				printf("\t\toperands[%u].type: REG_PAIR = (%s, %s)\n", i,
+					cs_reg_name(handle, op->reg_pair.reg_0),
+					cs_reg_name(handle, op->reg_pair.reg_1));
+				break;
 		}
 	}
 
