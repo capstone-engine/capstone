eb4dcfb214a3e982ab3b503afc1904809c2fb738
Nguyen Anh Quynh <aquynh@gmail.com>
2019-03-16 15:22:15 +0800

    arm: sync with llvm 7.0.1

--- arch/ARM/ARMDisassembler.h
+++ arch/ARM/ARMDisassembler.h
@@ -1,5 +1,5 @@
 /* Capstone Disassembly Engine */
-/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2015 */
+/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2019 */
 
 #ifndef CS_ARMDISASSEMBLER_H
 #define CS_ARMDISASSEMBLER_H
@@ -13,6 +13,6 @@ bool ARM_getInstruction(csh handle, const uint8_t *code, size_t code_len, MCInst
 
 bool Thumb_getInstruction(csh handle, const uint8_t *code, size_t code_len, MCInst *instr, uint16_t *size, uint64_t address, void *info);
 
-uint64_t ARM_getFeatureBits(unsigned int mode);
+bool ARM_getFeatureBits(unsigned int mode, unsigned int feature);
 
 #endif
--- arch/ARM/ARMMapping.h
+++ arch/ARM/ARMMapping.h
@@ -1,5 +1,5 @@
 /* Capstone Disassembly Engine */
-/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2015 */
+/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2019 */
 
 #ifndef CS_ARM_MAP_H
 #define CS_ARM_MAP_H
@@ -28,5 +28,22 @@ uint8_t *ARM_get_op_access(cs_struct *h, unsigned int id);
 void ARM_reg_access(const cs_insn *insn,
 		cs_regs regs_read, uint8_t *regs_read_count,
 		cs_regs regs_write, uint8_t *regs_write_count);
+
+struct IndexType {
+	uint16_t encoding;
+	unsigned index;
+};
+
+// binary search for encoding in IndexType array
+// return -1 if not found, or index if found
+unsigned int binsearch_IndexType(const struct IndexType *index, size_t size, uint16_t encoding);
+
+typedef struct BankedReg {
+	const char *Name;
+	arm_sysreg sysreg;
+	uint16_t Encoding;
+} BankedReg;
+
+BankedReg *lookupBankedRegByEncoding(uint8_t encoding);
 
 #endif
