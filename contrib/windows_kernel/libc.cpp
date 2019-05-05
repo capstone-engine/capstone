094cd203b66fb98b0b2eca7fbbb1417d6da6f1f1
Michael Bikovitsky <biko@bikodbg.com>
2019-05-05 09:59:52 +0300

    Fix user data alignment in MEMBLOCK
    Kernel memory allocations on Windows should be aligned on MEMORY_ALLOCATION_ALIGNMENT (16 bytes on x64 and 8 bytes on x86).

--- contrib/windows_kernel/libc.cpp
+++ contrib/windows_kernel/libc.cpp
@@ -20,6 +20,7 @@ struct MEMBLOCK
 	size_t	size;
 #pragma warning(push)               
 #pragma warning (disable : 4200)
+	__declspec(align(MEMORY_ALLOCATION_ALIGNMENT))
 	char data[0]; 
 #pragma warning(pop)
 };
