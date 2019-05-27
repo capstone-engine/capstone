bb22322793da860dde570c91ddcc890ceb05d4c7
Kelvin Sherlock <ksherlock@gmail.com>
2019-05-27 16:58:53 -0400

    updated 6502 support. some improvements to the base 6502 support but also adds support for 65c02, w65c02, and 65816.

---
+++ arch/MOS65XX/MOS65XXDisassemblerInternals.h
@@ -0,0 +1,22 @@
+#ifndef CS_MOS65XXDISASSEMBLERINTERNALS_H
+#define CS_MOS65XXDISASSEMBLERINTERNALS_H
+
+#include "capstone/mos65xx.h"
+
+enum {
+	MOS65XX_CPU_TYPE_6502,
+	MOS65XX_CPU_TYPE_65C02,
+	MOS65XX_CPU_TYPE_W65C02,
+	MOS65XX_CPU_TYPE_65816,
+};
+
+typedef struct mos65xx_info {
+
+	unsigned cpu_type;
+	unsigned long_m;
+	unsigned long_x;
+
+} mos65xx_info;
+
+
+#endif
