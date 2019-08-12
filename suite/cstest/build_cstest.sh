5bf7604e84049396f8584b16cfa01fec38b8a413
Stephen <stephen@egroat.com>
2019-08-12 15:14:31 -0700

    cleanup travis

--- suite/cstest/build_cstest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd cmocka && mkdir build && cd build
-if [ "$(uname)" = Darwin ]; then
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j2 && sudo make install
-else  # Linux
-cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make -j2 && sudo make install
-fi
-cd ../.. && make
