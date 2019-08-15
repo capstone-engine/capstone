c25923bc99fe746686aec571d10fcb7cc9faf885
naq <aquynh@gmail.com>
2019-08-15 15:28:15 +0800

    tests: fix typo in test_detail.c

--- tests/test_detail.c
+++ tests/test_detail.c
@@ -79,7 +79,7 @@ static void test()
 			CS_ARCH_X86,
 			(cs_mode)CS_MODE_16,
 			(unsigned char *)X86_CODE16,
-			sizeof(X86_CODE32) - 1,
+			sizeof(X86_CODE16) - 1,
 			"X86 16bit (Intel syntax)"
 		},
 		{
