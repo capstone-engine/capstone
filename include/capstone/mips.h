bcaff64b3bfce90c483db31fa1a8bca4c7ac797e
junchao <junchao_zhao@yeah.net>
2020-08-06 09:08:02 +0800

    fix CS_ mips_ OP structure comment error

--- include/capstone/mips.h
+++ include/capstone/mips.h
@@ -240,7 +240,7 @@ typedef struct mips_op_mem {
 typedef struct cs_mips_op {
 	mips_op_type type;	///< operand type
 	union {
-		mips_reg reg;		///< register value for REG operand
+		mips_reg reg;		///< register id for REG operand
 		int64_t imm;		///< immediate value for IMM operand
 		mips_op_mem mem;	///< base/index/scale/disp value for MEM operand
 	};
