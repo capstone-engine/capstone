capstone-engine/capstone

commit 0932f167fd5620bba1619fc3fae90924292ebb2e

Bug solved: SSE variant of MOVSD incorrectly decoded as REPNE MOVSD (#1540)

arch/X86/X86Mapping.c
@@ -3099,7 +3099,6 @@ static bool valid_repne(cs_struct *h, unsigned int opcode)
 
 			case X86_INS_MOVSB:
 			case X86_INS_MOVSW:
-			case X86_INS_MOVSD:
 			case X86_INS_MOVSQ:
 
 			case X86_INS_LODSB:
@@ -3121,6 +3120,11 @@ static bool valid_repne(cs_struct *h, unsigned int opcode)
 			case X86_INS_OUTSD:
 
 				return true;
+
+			case X86_INS_MOVSD:
+				if (opcode == X86_MOVSW) // REP MOVSB
+					return true;
+				return false;
 
 			case X86_INS_CMPSD:
 				if (opcode == X86_CMPSL) // REP CMPSD

commit e4fef1e92077e52738b91a66f7ff5435814ba96c

Fix bug introduced in https://github.com/aquynh/capstone/commit/0932f167fd5620bba1619fc3fae90924292ebb2e

arch/X86/X86Mapping.c
@@ -3122,7 +3122,7 @@ static bool valid_repne(cs_struct *h, unsigned int opcode)
 				return true;
 
 			case X86_INS_MOVSD:
-				if (opcode == X86_MOVSW) // REP MOVSB
+				if (opcode == X86_MOVSL) // REP MOVSD
 					return true;
 				return false;
 