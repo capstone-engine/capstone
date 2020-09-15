c8509846694c15efdd21caba33a45445474b83ed
StalkR <stalkr@stalkr.net>
2020-09-15 17:44:39 +0200

    ppc: fix registers overflow
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=22236

--- arch/PowerPC/PPCDisassembler.c
+++ arch/PowerPC/PPCDisassembler.c
@@ -164,9 +164,11 @@ static uint64_t getFeatureBits(int feature)
 }
 
 static DecodeStatus decodeRegisterClass(MCInst *Inst, uint64_t RegNo,
-		const unsigned *Regs)
+		const unsigned *Regs, size_t RegsLen)
 {
-	// assert(RegNo < N && "Invalid register number");
+	if (RegNo >= RegsLen / sizeof(unsigned)) {
+		return MCDisassembler_Fail;
+	}
 	MCOperand_CreateReg0(Inst, Regs[RegNo]);
 	return MCDisassembler_Success;
 }
@@ -175,70 +177,70 @@ static DecodeStatus DecodeCRRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, CRRegs);
+	return decodeRegisterClass(Inst, RegNo, CRRegs, sizeof(CRRegs));
 }
 
 static DecodeStatus DecodeCRBITRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, CRBITRegs);
+	return decodeRegisterClass(Inst, RegNo, CRBITRegs, sizeof(CRBITRegs));
 }
 
 static DecodeStatus DecodeF4RCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, FRegs);
+	return decodeRegisterClass(Inst, RegNo, FRegs, sizeof(FRegs));
 }
 
 static DecodeStatus DecodeF8RCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, FRegs);
+	return decodeRegisterClass(Inst, RegNo, FRegs, sizeof(FRegs));
 }
 
 static DecodeStatus DecodeVRRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, VRegs);
+	return decodeRegisterClass(Inst, RegNo, VRegs, sizeof(VRegs));
 }
 
 static DecodeStatus DecodeVSRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, VSRegs);
+	return decodeRegisterClass(Inst, RegNo, VSRegs, sizeof(VSRegs));
 }
 
 static DecodeStatus DecodeVSFRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, VSFRegs);
+	return decodeRegisterClass(Inst, RegNo, VSFRegs, sizeof(VSFRegs));
 }
 
 static DecodeStatus DecodeGPRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, GPRegs);
+	return decodeRegisterClass(Inst, RegNo, GPRegs, sizeof(GPRegs));
 }
 
 static DecodeStatus DecodeGPRC_NOR0RegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, GP0Regs);
+	return decodeRegisterClass(Inst, RegNo, GP0Regs, sizeof(GP0Regs));
 }
 
 static DecodeStatus DecodeG8RCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, G8Regs);
+	return decodeRegisterClass(Inst, RegNo, G8Regs, sizeof(G8Regs));
 }
 
 #define DecodePointerLikeRegClass0 DecodeGPRCRegisterClass
@@ -248,7 +250,7 @@ static DecodeStatus DecodeQFRCRegisterClass(MCInst *Inst, uint64_t RegNo,
 		uint64_t Address,
 		const void *Decoder)
 {
-	return decodeRegisterClass(Inst, RegNo, QFRegs);
+	return decodeRegisterClass(Inst, RegNo, QFRegs, sizeof(QFRegs));
 }
 
 #define DecodeQSRCRegisterClass DecodeQFRCRegisterClass
