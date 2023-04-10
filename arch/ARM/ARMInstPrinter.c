1621f49d8bba6b11bd487ecc5dc38a75b38a1878
Nam Cao <namcaov@gmail.com>
2023-04-10 08:34:15 +0200

    correct register access attributes of Arm's LDM instruction

--- arch/ARM/ARMInstPrinter.c
+++ arch/ARM/ARMInstPrinter.c
@@ -753,13 +753,14 @@ void ARM_printInst(MCInst *MI, SStream *O, void *Info)
 				if (MI->csh->detail) {
 					MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].type = ARM_OP_REG;
 					MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].reg = BaseReg;
-					MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].access = CS_AC_READ | CS_AC_WRITE;
+					MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].access = CS_AC_READ;
 					MI->flat_insn->detail->arm.op_count++;
 				}
 
 				if (Writeback) {
 					MI->writeback = true;
 					SStream_concat0(O, "!");
+					MI->flat_insn->detail->arm.operands[MI->flat_insn->detail->arm.op_count].access |= CS_AC_WRITE;
 				}
 
 				SStream_concat0(O, ", ");
