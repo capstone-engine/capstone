932bd3bef10fdd642c9ab317bf056a397e7cf292
billow <billow.fun@gmail.com>
2023-05-13 03:37:08 +0800

    Update arch/TriCore/TriCoreInstPrinter.c
    Co-authored-by: Rot127 <45763064+Rot127@users.noreply.github.com>

--- arch/TriCore/TriCoreInstPrinter.c
+++ arch/TriCore/TriCoreInstPrinter.c
@@ -336,7 +336,7 @@ static void printDisp15Imm(MCInst *MI, int OpNum, SStream *O)
 {
 	MCOperand *MO = MCInst_getOperand(MI, OpNum);
 	if (MCOperand_isImm(MO)) {
-		uint32_t disp = (int32_t)MCOperand_getImm(MO);
+		uint32_t disp = MCOperand_getImm(MO);
 		switch (MCInst_getOpcode(MI)) {
 		case TRICORE_JEQ_brc:
 		case TRICORE_JEQ_brr:
