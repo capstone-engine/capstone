8767be16733297cc3fc80e3a2fda74015d68bb02
Rot127 <unisono@quyllur.org>
2023-05-20 08:55:18 -0500

    Handle qualified identifiers without namespace.

--- suite/auto-sync/CppTranslator/Patches/QualifiedIdentifier.py
+++ suite/auto-sync/CppTranslator/Patches/QualifiedIdentifier.py
@@ -20,8 +20,14 @@ class QualifiedIdentifier(Patch):
         return "qualified_id"
 
     def get_patch(self, captures: [(Node, str)], src: bytes, **kwargs) -> bytes:
-        namespace = captures[0][0].named_children[0]
-        identifier = captures[0][0].named_children[1]
-        namespace = get_text(src, namespace.start_byte, namespace.end_byte)
-        identifier = get_text(src, identifier.start_byte, identifier.end_byte)
+        if len(captures[0][0].named_children) > 1:
+            identifier = captures[0][0].named_children[1]
+            identifier = get_text(src, identifier.start_byte, identifier.end_byte)
+            namespace = captures[0][0].named_children[0]
+            namespace = get_text(src, namespace.start_byte, namespace.end_byte)
+        else:
+            # The namespace can be omitted. E.g. std::transform(..., ::tolower)
+            namespace = b""
+            identifier = captures[0][0].named_children[0]
+            identifier = get_text(src, identifier.start_byte, identifier.end_byte)
         return namespace + b"_" + identifier
