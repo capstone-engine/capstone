c041b5ab6ca700fc2f96b97f01bc4497cd825729
Rot127 <unisono@quyllur.org>
2023-05-31 09:39:21 -0500

    Fix getReg/getImm patch to not patch other functions with getReg in name

--- suite/auto-sync/CppTranslator/Patches/GetOperandRegImm.py
+++ suite/auto-sync/CppTranslator/Patches/GetOperandRegImm.py
@@ -1,6 +1,6 @@
 from tree_sitter import Node
 
-from Patches.HelperMethods import get_text
+from Patches.HelperMethods import get_text, get_capture_node
 from Patches.Patch import Patch
 
 
@@ -22,7 +22,7 @@ class GetOperandRegImm(Patch):
             "        ((_) @operand)"
             '        ((field_identifier) @field_id (#match? @field_id "get(Reg|Imm)"))'
             "    )"
-            "    (argument_list)"
+            '    ((argument_list) @arg_list (#eq? @arg_list "()"))'
             ") @get_operand"
         )
         return q
@@ -32,9 +32,9 @@ class GetOperandRegImm(Patch):
 
     def get_patch(self, captures: [(Node, str)], src: bytes, **kwargs) -> bytes:
         # The operand
-        operand: Node = captures[1][0]
+        operand: Node = get_capture_node(captures, "operand")
         # 'getReg()/getImm()'
-        get_reg_imm = captures[2][0]
+        get_reg_imm = get_capture_node(captures, "field_id")
 
         fcn = get_text(src, get_reg_imm.start_byte, get_reg_imm.end_byte)
         op = get_text(src, operand.start_byte, operand.end_byte)
