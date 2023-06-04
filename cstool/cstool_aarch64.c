cc1a3db792da1f136e407695fae231f93f5167d0
Rot127 <unisono@quyllur.org>
2023-06-04 09:34:38 -0500

    Rename cstool_arm64 -> cstool_aarch64

--- cstool/cstool_aarch64.c
+++ cstool/cstool_aarch64.c
@@ -7,9 +7,9 @@
 #include <capstone/capstone.h>
 #include "cstool.h"
 
-void print_insn_detail_arm64(csh handle, cs_insn *ins)
+void print_insn_detail_aarch64(csh handle, cs_insn *ins)
 {
-	cs_arm64 *arm64;
+	cs_aarch64 *aarch64;
 	int i;
 	cs_regs regs_read, regs_write;
 	uint8_t regs_read_count, regs_write_count;
@@ -19,22 +19,22 @@ void print_insn_detail_arm64(csh handle, cs_insn *ins)
 	if (ins->detail == NULL)
 		return;
 
-	arm64 = &(ins->detail->arm64);
-	if (arm64->op_count)
-		printf("\top_count: %u\n", arm64->op_count);
+	aarch64 = &(ins->detail->aarch64);
+	if (aarch64->op_count)
+		printf("\top_count: %u\n", aarch64->op_count);
 
-	for (i = 0; i < arm64->op_count; i++) {
-		cs_arm64_op *op = &(arm64->operands[i]);
+	for (i = 0; i < aarch64->op_count; i++) {
+		cs_aarch64_op *op = &(aarch64->operands[i]);
 		switch(op->type) {
 			default:
 				break;
-			case ARM64_OP_REG:
+			case AArch64_OP_REG:
 				printf("\t\toperands[%u].type: REG = %s\n", i, cs_reg_name(handle, op->reg));
 				break;
-			case ARM64_OP_IMM:
+			case AArch64_OP_IMM:
 				printf("\t\toperands[%u].type: IMM = 0x%" PRIx64 "\n", i, op->imm);
 				break;
-			case ARM64_OP_FP:
+			case AArch64_OP_FP:
 #if defined(_KERNEL_MODE)
 				// Issue #681: Windows kernel does not support formatting float point
 				printf("\t\toperands[%u].type: FP = <float_point_unsupported>\n", i);
@@ -42,49 +42,49 @@ void print_insn_detail_arm64(csh handle, cs_insn *ins)
 				printf("\t\toperands[%u].type: FP = %f\n", i, op->fp);
 #endif
 				break;
-			case ARM64_OP_MEM:
+			case AArch64_OP_MEM:
 				printf("\t\toperands[%u].type: MEM\n", i);
-				if (op->mem.base != ARM64_REG_INVALID)
+				if (op->mem.base != AArch64_REG_INVALID)
 					printf("\t\t\toperands[%u].mem.base: REG = %s\n", i, cs_reg_name(handle, op->mem.base));
-				if (op->mem.index != ARM64_REG_INVALID)
+				if (op->mem.index != AArch64_REG_INVALID)
 					printf("\t\t\toperands[%u].mem.index: REG = %s\n", i, cs_reg_name(handle, op->mem.index));
 				if (op->mem.disp != 0)
 					printf("\t\t\toperands[%u].mem.disp: 0x%x\n", i, op->mem.disp);
 
 				break;
-			case ARM64_OP_CIMM:
+			case AArch64_OP_CIMM:
 				printf("\t\toperands[%u].type: C-IMM = %u\n", i, (int)op->imm);
 				break;
-			case ARM64_OP_REG_MRS:
+			case AArch64_OP_REG_MRS:
 				printf("\t\toperands[%u].type: REG_MRS = 0x%x\n", i, op->reg);
 				break;
-			case ARM64_OP_REG_MSR:
+			case AArch64_OP_REG_MSR:
 				printf("\t\toperands[%u].type: REG_MSR = 0x%x\n", i, op->reg);
 				break;
-			case ARM64_OP_PSTATE:
+			case AArch64_OP_PSTATE:
 				printf("\t\toperands[%u].type: PSTATE = 0x%x\n", i, op->pstate);
 				break;
-			case ARM64_OP_SYS:
+			case AArch64_OP_SYS:
 				printf("\t\toperands[%u].type: SYS = 0x%x\n", i, op->sys);
 				break;
-			case ARM64_OP_PREFETCH:
+			case AArch64_OP_PREFETCH:
 				printf("\t\toperands[%u].type: PREFETCH = 0x%x\n", i, op->prefetch);
 				break;
-			case ARM64_OP_BARRIER:
+			case AArch64_OP_BARRIER:
 				printf("\t\toperands[%u].type: BARRIER = 0x%x\n", i, op->barrier);
 				break;
-			case ARM64_OP_SVCR:
+			case AArch64_OP_SVCR:
 				printf("\t\toperands[%u].type: SYS = 0x%x\n", i, op->sys);
-				if(op->svcr == ARM64_SVCR_SVCRSM)
+				if(op->svcr == AArch64_SVCR_SVCRSM)
 					printf("\t\t\toperands[%u].svcr: BIT = SM\n", i);
-				if(op->svcr == ARM64_SVCR_SVCRZA)
+				if(op->svcr == AArch64_SVCR_SVCRZA)
 					printf("\t\t\toperands[%u].svcr: BIT = ZA\n", i);
-				if(op->svcr == ARM64_SVCR_SVCRSMZA)
+				if(op->svcr == AArch64_SVCR_SVCRSMZA)
 					printf("\t\t\toperands[%u].svcr: BIT = SM & ZA\n", i);
 				break;
-			case ARM64_OP_SME_INDEX:
+			case AArch64_OP_SME_INDEX:
 				printf("\t\toperands[%u].type: REG = %s\n", i, cs_reg_name(handle, op->sme_index.reg));
-				if (op->sme_index.base != ARM64_REG_INVALID)
+				if (op->sme_index.base != AArch64_REG_INVALID)
 					printf("\t\t\toperands[%u].index.base: REG = %s\n", i, cs_reg_name(handle, op->sme_index.base));
 				if (op->sme_index.disp != 0)
 					printf("\t\t\toperands[%u].index.disp: 0x%x\n", i, op->sme_index.disp);
@@ -106,29 +106,29 @@ void print_insn_detail_arm64(csh handle, cs_insn *ins)
 				break;
 		}
 		
-		if (op->shift.type != ARM64_SFT_INVALID &&
+		if (op->shift.type != AArch64_SFT_INVALID &&
 			op->shift.value)
 			printf("\t\t\tShift: type = %u, value = %u\n",
 				   op->shift.type, op->shift.value);
 
-		if (op->ext != ARM64_EXT_INVALID)
+		if (op->ext != AArch64_EXT_INVALID)
 			printf("\t\t\tExt: %u\n", op->ext);
 
-		if (op->vas != ARM64_VAS_INVALID)
+		if (op->vas != AArch64_VAS_INVALID)
 			printf("\t\t\tVector Arrangement Specifier: 0x%x\n", op->vas);
 
 		if (op->vector_index != -1)
 			printf("\t\t\tVector Index: %u\n", op->vector_index);
 	}
 
-	if (arm64->update_flags)
+	if (aarch64->update_flags)
 		printf("\tUpdate-flags: True\n");
 
-	if (arm64->writeback)
+	if (aarch64->writeback)
 		printf("\tWrite-back: True\n");
 
-	if (arm64->cc)
-		printf("\tCode-condition: %u\n", arm64->cc);
+	if (aarch64->cc)
+		printf("\tCode-condition: %u\n", aarch64->cc);
 
 	// Print out all registers accessed by this instruction (either implicit or explicit)
 	if (!cs_regs_access(handle, ins,
