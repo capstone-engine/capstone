5e0c3464f6e7d8a9a8f1fb29a2a0f9ae44fc8cda
kabeor <kabeor00@gmail.com>
2023-07-21 22:34:30 +0800

    fix str

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -203,7 +203,7 @@ if 'bdist_wheel' in sys.argv and '--plat-name' not in sys.argv:
     name = get_platform()
     pyversion = platform.python_version()
     major_version, minor_version = map(int, pyversion.split('.')[:2])
-    sys.argv.insert(idx + 1, name.replace('.', '_').replace('-', '_') + "_" + major_version + minor_version)
+    sys.argv.insert(idx + 1, name.replace('.', '_').replace('-', '_') + "_" + str(major_version) + str(minor_version))
 
 setup(
     provides=['capstone'],
