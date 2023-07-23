1bbb61081260c5c0a8970f069950a2130a474285
Rot127 <unisono@quyllur.org>
2023-07-23 08:54:56 -0500

    Handle system operands in way more detail.
    - Differentiates between banked and non-banked registers.
    - Saves the MSR mask.
    - PSR field bits are saved separately.
    - SYSm value is saved if possible.

--- cstool/cstool_arm.c
+++ cstool/cstool_arm.c
@@ -1,3 +1,4 @@
+#include "capstone/arm.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -70,9 +71,37 @@ void print_insn_detail_arm(csh handle, cs_insn *ins)
 			case ARM_OP_SETEND:
 				printf("\t\toperands[%u].type: SETEND = %s\n", i, op->setend == ARM_SETEND_BE? "be" : "le");
 				break;
-			case ARM_OP_SYSREG:
-				printf("\t\toperands[%u].type: SYSREG = %s\n", i, cs_reg_name(handle, op->reg));
+			case ARM_OP_SYSM:
+				printf("\t\toperands[%u].type: SYSM = 0x%" PRIx16 "\n", i, op->sysop.sysm);
+				printf("\t\toperands[%u].type: MASK = %" PRIu8 "\n", i, op->sysop.msr_mask);
 				break;
+			case ARM_OP_SYSREG:
+				printf("\t\toperands[%u].type: SYSREG = %s\n", i, cs_reg_name(handle, (uint32_t) op->sysop.reg.mclasssysreg));
+				printf("\t\toperands[%u].type: MASK = %" PRIu8 "\n", i, op->sysop.msr_mask);
+				break;
+			case ARM_OP_BANKEDREG:
+				// FIXME: Printing the name is currenliy not supported if the encodings overlap
+				// with system registers.
+				printf("\t\toperands[%u].type: BANKEDREG = %" PRIu32 "\n", i, (uint32_t) op->sysop.reg.bankedreg);
+				if (op->sysop.msr_mask != UINT8_MAX)
+					printf("\t\toperands[%u].type: MASK = %" PRIu8 "\n", i, op->sysop.msr_mask);
+			case ARM_OP_SPSR:
+			case ARM_OP_CPSR: {
+				const char type = op->type == ARM_OP_SPSR ? 'S' : 'C';
+				printf("\t\toperands[%u].type: %cPSR = ", i, type);
+				uint16_t field = op->sysop.psr_bits;
+				if ((field & ARM_FIELD_SPSR_F) || (field & ARM_FIELD_CPSR_F))
+					printf("f");
+				if ((field & ARM_FIELD_SPSR_S) || (field & ARM_FIELD_CPSR_S))
+					printf("s");
+				if ((field & ARM_FIELD_SPSR_X) || (field & ARM_FIELD_CPSR_X))
+					printf("x");
+				if ((field & ARM_FIELD_SPSR_C) || (field & ARM_FIELD_CPSR_C))
+					printf("c");
+				printf("\n");
+				printf("\t\toperands[%u].type: MASK = %" PRIu8 "\n", i, op->sysop.msr_mask);
+				break;
+			}
 		}
 
 		if (op->neon_lane != -1) {
