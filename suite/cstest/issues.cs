83138425a8cfe84bbfe7369cd02e891e4d2659b7
Rot127 <unisono@quyllur.org>
2023-08-09 08:04:46 -0500

    Fix syntax/alias issuesin issues.cs

--- suite/cstest/issues.cs
+++ suite/cstest/issues.cs
@@ -287,17 +287,17 @@
 
 !# issue 970 PPC bdnzt lt
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, CS_OPT_DETAIL
-0x41,0x00,0xff,0xac == bdnzt lt, 0xffffffffffffffac ; operands[0].type: REG = cr0lt
+0x41,0x00,0xff,0xac == bdnzt lt, 0xffffffffffffffac ; operands[0].type: REG = 0
 
 !# issue 970 PPC bdnzt eq
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, CS_OPT_DETAIL
-0x41,0x02,0xff,0xac == bdnzt eq, 0xffffffffffffffac ; operands[0].type: REG = cr0eq
+0x41,0x02,0xff,0xac == bdnzt eq, 0xffffffffffffffac ; operands[0].type: REG = 2
 
 !# issue 969 PPC bdnzflr operand 2
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, CS_OPT_DETAIL
-0x4c,0x10,0x00,0x20 == bdnzflr 4*cr4+lt ; operands[0].type: REG = cr4lt
+0x4c,0x10,0x00,0x20 == bdnzflr 4*cr4+lt ; operands[0].type: REG = 16
 
-0x41,0x82,0x00,0x10 == beq 0x10 ; Groups: jump
+0x41,0x82,0x00,0x10 == bt eq, 0x10 ; Groups: jump
 
 !# issue 1481 ARM64 LDR operand2
 !# CS_ARCH_ARM64, CS_MODE_LITTLE_ENDIAN, CS_OPT_DETAIL
@@ -305,11 +305,11 @@
 
 !# issue 968 PPC absolute branch: bdnzla
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, None
-0x1000: 0x42,0x00,0x12,0x37 == bdnzla 0x1234
+0x1000: 0x42,0x00,0x12,0x37 == bcla 0x10, lt, 0x1234
 
 !# issue 968 PPC absolute branch: bdzla
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, None
-0x1000: 0x42,0x40,0x12,0x37 == bdzla 0x1234
+0x1000: 0x42,0x40,0x12,0x37 == bcla	0x12, lt, 0x1234
 
 !# issue X86 xrelease xchg
 !# CS_ARCH_X86, CS_MODE_32, None
@@ -329,11 +329,11 @@
 
 !# issue PPC JUMP group
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, CS_OPT_DETAIL
-0x41,0x82,0x00,0x10 == beq 0x10 ; Groups: jump
+0x41,0x82,0x00,0x10 == bt eq, 0x10 ; Groups: jump branch_relative
 
 !# issue 1468 PPC bdnz
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, None
-0x101086c: 0x42,0x00,0xff,0xf8 == bdnz 0x1010864
+0x101086c: 0x42,0x00,0xff,0xf8 == bc 0x10, lt, 0x1010864
 
 !# issue PPC bdnzt
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, None
@@ -341,7 +341,7 @@
 
 !# issue 1469 PPC CRx
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, CS_OPT_DETAIL
-0x4c,0x02,0x39,0x82 == crxor cr0lt, cr0eq, cr1un ; operands[0].type: REG = cr0lt
+0x4c,0x02,0x39,0x82 == crxor lt, eq, 4*cr1+un ; operands[0].type: REG = 0 ; operands[1].type: REG = 2 ; operands[2].type: REG = 7
 
 !# issue 1468 B target
 !# CS_ARCH_PPC, CS_MODE_64 | CS_MODE_BIG_ENDIAN, None
