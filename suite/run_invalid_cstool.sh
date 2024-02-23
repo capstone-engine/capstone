122348ba471c26f73872c597da721f54b0c32e79
Rot127 <45763064+Rot127@users.noreply.github.com>
2024-02-23 05:20:00 +0000

    [CMake] Architecture definition fix (#2272)
    * Enble all archs only if CAPSTONE_USE_ARCH_REGISTRATION=false
    
    * Add a test if cstool reaches each disassembler engine.
    
    * Move cstool test to sh file.

---
+++ suite/run_invalid_cstool.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cstool -d x64 0x4 | grep "ERROR: invalid assembly code" &&
+cstool -d arm 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d aarch64 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d alpha 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d mips64 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d ppc64 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d sparc 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d systemz 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d xcore 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d m68k 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d tms320c64x 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d m6811 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d cpu12 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d hd6309 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d hcs08 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d evm 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d 6502 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d 65c02 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d w65c02 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d 65816 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d wasm 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d bpf 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d ebpf 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d riscv64 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d sh 0x1 | grep "ERROR: invalid assembly code" &&
+cstool -d tc162 0x1 | grep "ERROR: invalid assembly code"
+
