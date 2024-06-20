f54762e7b2abf00b59e382da66ff356462f3efbd
Jiajie Chen <c@jia.je>
2024-06-20 08:29:12 +0800

    Capticalize enums in loongarch_detail.c

--- suite/cstest/src/loongarch_detail.c
+++ suite/cstest/src/loongarch_detail.c
@@ -26,18 +26,18 @@ char *get_detail_loongarch(csh *handle, cs_mode mode, cs_insn *ins)
 		switch((int)op->type) {
 			default:
 				break;
-			case LoongArch_OP_REG:
+			case LOONGARCH_OP_REG:
 				add_str(&result, " ; operands[%u].type: REG = %s", i, cs_reg_name(*handle, op->reg));
 				break;
-			case LoongArch_OP_IMM:
+			case LOONGARCH_OP_IMM:
 				add_str(&result, " ; operands[%u].type: IMM = 0x%x", i, op->imm);
 				break;
-			case LoongArch_OP_MEM:
+			case LOONGARCH_OP_MEM:
 				add_str(&result, " ; operands[%u].type: MEM", i);
-				if (op->mem.base != LoongArch_REG_INVALID)
+				if (op->mem.base != LOONGARCH_REG_INVALID)
 					add_str(&result, " ; operands[%u].mem.base: REG = %s",
 							i, cs_reg_name(*handle, op->mem.base));
-				if (op->mem.index != LoongArch_REG_INVALID)
+				if (op->mem.index != LOONGARCH_REG_INVALID)
 					add_str(&result, " ; operands[%u].mem.index: REG = %s",
 							i, cs_reg_name(*handle, op->mem.index));
 				if (op->mem.disp != 0)
