95f82446c17af115c90aa2b94574ba4e122f890f
Kevin Phoenix <kevin@kphoenix.us>
2024-07-02 12:32:56 -0700

    Remove PurePath() call breaking .join()

--- bindings/python/capstone/__init__.py
+++ bindings/python/capstone/__init__.py
@@ -437,7 +437,7 @@ _path_list = [os.getenv('LIBCAPSTONE_PATH', None),
               resources.files(__name__) / "lib",
               join(split(__file__)[0], 'lib'),
               '',
-              PurePath(sysconfig.get_path('platlib')),
+              sysconfig.get_path('platlib'),
               "/usr/local/lib/" if sys.platform == 'darwin' else '/usr/lib64']
 
 for _path in _path_list:
