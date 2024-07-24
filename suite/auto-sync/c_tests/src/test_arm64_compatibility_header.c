b34864d1de583464a9a8aca19a391ab6f0fa583c
Rot127 <unisono@quyllur.org>
2024-07-24 03:29:42 -0500

    Add compatibility header test to CMake and CI

--- suite/auto-sync/c_tests/src/test_arm64_compatibility_header.c
+++ suite/auto-sync/c_tests/src/test_arm64_compatibility_header.c
@@ -12,7 +12,7 @@ int main(void)
 	csh handle;
 
 	if (cs_open(CS_ARCH_ARM64, CS_MODE_BIG_ENDIAN, &handle) != CS_ERR_OK) {
-		printf("cs_open failed\n");
+		fprintf(stderr, "cs_open failed\n");
 		return -1;
 	}
 
@@ -20,21 +20,48 @@ int main(void)
 
 	cs_insn *insn;
 	uint8_t bytes[] = "0x1a,0x48,0xa0,0xf8";
-	size_t count = cs_disasm(handle, bytes, sizeof(bytes), 0x1000, 1, &insn);
-	if (count > 0) {
-		printf("0x%" PRIx64 ":\t%s\t\t%s\n", insn[0].address,
-		       insn[0].mnemonic, insn[0].op_str);
-		printf("A register = %s\n", cs_reg_name(handle, insn[0].detail->arm64.operands[0].reg));
-		printf("An imm = 0x%" PRIx64 "\n", insn[0].detail->arm64.operands[1].imm);
+	size_t count =
+		cs_disasm(handle, bytes, sizeof(bytes), 0x1000, 1, &insn);
+	if (count != 1) {
+		fprintf(stderr, "Failed to disassemble code.\n");
+		goto err;
+	}
+	printf("0x%" PRIx64 ":\t%s\t\t%s\n", insn[0].address, insn[0].mnemonic,
+	       insn[0].op_str);
+	printf("A register = %s\n",
+	       cs_reg_name(handle, insn[0].detail->arm64.operands[0].reg));
+	printf("An imm = 0x%" PRIx64 "\n",
+	       insn[0].detail->arm64.operands[1].imm);
 
-		cs_free(insn, count);
-	} else {
-		printf("ERROR: Failed to disassemble given code!\n");
-		cs_close(&handle);
-		return -1;
+	if (insn[0].address != 0x1000) {
+		fprintf(stderr, "Address wrong.\n");
+		goto err;
+	}
+	if (strcmp(insn[0].mnemonic, "adr") != 0) {
+		fprintf(stderr, "Mnemonic wrong.\n");
+		goto err;
+	}
+	if (strcmp(insn[0].op_str, "x1, 0xf162d") != 0) {
+		fprintf(stderr, "op_str wrong.\n");
+		goto err;
+	}
+	if (strcmp(cs_reg_name(handle, insn[0].detail->arm64.operands[0].reg),
+		   "x1") != 0) {
+		fprintf(stderr, "register wrong.\n");
+		goto err;
+	}
+	if (insn[0].detail->arm64.operands[1].imm != 0xf162d) {
+		fprintf(stderr, "Immediate wrong.\n");
+		goto err;
 	}
 
+	cs_free(insn, count);
 	cs_close(&handle);
-
 	return 0;
+
+err:
+	printf("ERROR: Failed to disassemble given code corrcetly!\n");
+	cs_free(insn, count);
+	cs_close(&handle);
+	return -1;
 }
