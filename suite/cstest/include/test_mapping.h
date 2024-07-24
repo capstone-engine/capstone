73cfc86701095c115550ef04d327c64f8b5e2ba8
Rot127 <unisono@quyllur.org>
2024-07-24 02:19:33 -0500

    Formatting

--- suite/cstest/include/test_mapping.h
+++ suite/cstest/include/test_mapping.h
@@ -8,71 +8,71 @@
 
 /// Maps a string to the CS_ARCH enum
 typedef struct {
-  const char *str;
-  cs_arch arch;
+	const char *str;
+	cs_arch arch;
 } TestArchMapEntry;
 
 /// Maps a string to the CS_MODE enum
 typedef struct {
-  const char *str;
-  cs_mode mode;
+	const char *str;
+	cs_mode mode;
 } TestModeMapEntry;
 
 /// Maps a string to an option
 typedef struct {
-  const char *str;
-  cs_opt opt;
+	const char *str;
+	cs_opt opt;
 } TestOptionMapEntry;
 
 /// A map entry, mapping a Capstone enumeration value id
 /// to its absolute value.
 typedef struct {
-  const char *id;
-  uint32_t val;
+	const char *id;
+	uint32_t val;
 } TestCSEnumEntry;
 
 static const TestArchMapEntry test_arch_map[] = {
 	{ .str = "CS_ARCH_ARM", .arch = CS_ARCH_ARM },
 	{ .str = "CS_ARCH_AARCH64", .arch = CS_ARCH_AARCH64 },
-	{ .str = "CS_ARCH_MIPS", .arch = CS_ARCH_MIPS },	
-	{ .str = "CS_ARCH_X86", .arch = CS_ARCH_X86 },	
-	{ .str = "CS_ARCH_PPC", .arch = CS_ARCH_PPC },	
-	{ .str = "CS_ARCH_SPARC", .arch = CS_ARCH_SPARC },	
-	{ .str = "CS_ARCH_SYSZ", .arch = CS_ARCH_SYSZ },	
-	{ .str = "CS_ARCH_XCORE", .arch = CS_ARCH_XCORE },	
-	{ .str = "CS_ARCH_M68K", .arch = CS_ARCH_M68K },	
+	{ .str = "CS_ARCH_MIPS", .arch = CS_ARCH_MIPS },
+	{ .str = "CS_ARCH_X86", .arch = CS_ARCH_X86 },
+	{ .str = "CS_ARCH_PPC", .arch = CS_ARCH_PPC },
+	{ .str = "CS_ARCH_SPARC", .arch = CS_ARCH_SPARC },
+	{ .str = "CS_ARCH_SYSZ", .arch = CS_ARCH_SYSZ },
+	{ .str = "CS_ARCH_XCORE", .arch = CS_ARCH_XCORE },
+	{ .str = "CS_ARCH_M68K", .arch = CS_ARCH_M68K },
 	{ .str = "CS_ARCH_TMS320C64X", .arch = CS_ARCH_TMS320C64X },
-	{ .str = "CS_ARCH_M680X", .arch = CS_ARCH_M680X },	
-	{ .str = "CS_ARCH_EVM", .arch = CS_ARCH_EVM },	
+	{ .str = "CS_ARCH_M680X", .arch = CS_ARCH_M680X },
+	{ .str = "CS_ARCH_EVM", .arch = CS_ARCH_EVM },
 	{ .str = "CS_ARCH_MOS65XX", .arch = CS_ARCH_MOS65XX },
-	{ .str = "CS_ARCH_WASM", .arch = CS_ARCH_WASM },	
-	{ .str = "CS_ARCH_BPF", .arch = CS_ARCH_BPF },	
+	{ .str = "CS_ARCH_WASM", .arch = CS_ARCH_WASM },
+	{ .str = "CS_ARCH_BPF", .arch = CS_ARCH_BPF },
 	{ .str = "CS_ARCH_RISCV", .arch = CS_ARCH_RISCV },
 	{ .str = "CS_ARCH_SH", .arch = CS_ARCH_SH },
 	{ .str = "CS_ARCH_TRICORE", .arch = CS_ARCH_TRICORE },
-	{ .str = "CS_ARCH_ALPHA", .arch = CS_ARCH_ALPHA },	
-	{ .str = "CS_ARCH_HPPA", .arch = CS_ARCH_HPPA },	
+	{ .str = "CS_ARCH_ALPHA", .arch = CS_ARCH_ALPHA },
+	{ .str = "CS_ARCH_HPPA", .arch = CS_ARCH_HPPA },
 	{ .str = "CS_ARCH_LOONGARCH", .arch = CS_ARCH_LOONGARCH },
 	{ .str = "arm", .arch = CS_ARCH_ARM },
 	{ .str = "aarch64", .arch = CS_ARCH_AARCH64 },
-	{ .str = "mips", .arch = CS_ARCH_MIPS },	
-	{ .str = "x86", .arch = CS_ARCH_X86 },	
-	{ .str = "ppc", .arch = CS_ARCH_PPC },	
-	{ .str = "sparc", .arch = CS_ARCH_SPARC },	
-	{ .str = "sysz", .arch = CS_ARCH_SYSZ },	
-	{ .str = "xcore", .arch = CS_ARCH_XCORE },	
-	{ .str = "m68k", .arch = CS_ARCH_M68K },	
+	{ .str = "mips", .arch = CS_ARCH_MIPS },
+	{ .str = "x86", .arch = CS_ARCH_X86 },
+	{ .str = "ppc", .arch = CS_ARCH_PPC },
+	{ .str = "sparc", .arch = CS_ARCH_SPARC },
+	{ .str = "sysz", .arch = CS_ARCH_SYSZ },
+	{ .str = "xcore", .arch = CS_ARCH_XCORE },
+	{ .str = "m68k", .arch = CS_ARCH_M68K },
 	{ .str = "tms320c64x", .arch = CS_ARCH_TMS320C64X },
-	{ .str = "m680x", .arch = CS_ARCH_M680X },	
-	{ .str = "evm", .arch = CS_ARCH_EVM },	
+	{ .str = "m680x", .arch = CS_ARCH_M680X },
+	{ .str = "evm", .arch = CS_ARCH_EVM },
 	{ .str = "mos65xx", .arch = CS_ARCH_MOS65XX },
-	{ .str = "wasm", .arch = CS_ARCH_WASM },	
-	{ .str = "bpf", .arch = CS_ARCH_BPF },	
+	{ .str = "wasm", .arch = CS_ARCH_WASM },
+	{ .str = "bpf", .arch = CS_ARCH_BPF },
 	{ .str = "riscv", .arch = CS_ARCH_RISCV },
 	{ .str = "sh", .arch = CS_ARCH_SH },
 	{ .str = "tricore", .arch = CS_ARCH_TRICORE },
-	{ .str = "alpha", .arch = CS_ARCH_ALPHA },	
-	{ .str = "hppa", .arch = CS_ARCH_HPPA },	
+	{ .str = "alpha", .arch = CS_ARCH_ALPHA },
+	{ .str = "hppa", .arch = CS_ARCH_HPPA },
 	{ .str = "loongarch", .arch = CS_ARCH_LOONGARCH },
 	{ .str = "AArch64", .arch = CS_ARCH_AARCH64 },
 };
@@ -116,16 +116,19 @@ static const TestModeMapEntry test_mode_map[] = {
 	{ .str = "CS_MODE_M680X_HCS08", .mode = CS_MODE_M680X_HCS08 },
 	{ .str = "CS_MODE_BPF_CLASSIC", .mode = CS_MODE_BPF_CLASSIC },
 	{ .str = "CS_MODE_BPF_EXTENDED", .mode = CS_MODE_BPF_EXTENDED },
-	{ .str = "CS_MODE_RISCV32", .mode = CS_MODE_RISCV32  },
-	{ .str = "CS_MODE_RISCV64", .mode = CS_MODE_RISCV64  },
-	{ .str = "CS_MODE_RISCVC", .mode = CS_MODE_RISCVC   },
+	{ .str = "CS_MODE_RISCV32", .mode = CS_MODE_RISCV32 },
+	{ .str = "CS_MODE_RISCV64", .mode = CS_MODE_RISCV64 },
+	{ .str = "CS_MODE_RISCVC", .mode = CS_MODE_RISCVC },
 	{ .str = "CS_MODE_MOS65XX_6502", .mode = CS_MODE_MOS65XX_6502 },
 	{ .str = "CS_MODE_MOS65XX_65C02", .mode = CS_MODE_MOS65XX_65C02 },
 	{ .str = "CS_MODE_MOS65XX_W65C02", .mode = CS_MODE_MOS65XX_W65C02 },
 	{ .str = "CS_MODE_MOS65XX_65816", .mode = CS_MODE_MOS65XX_65816 },
-	{ .str = "CS_MODE_MOS65XX_65816_LONG_M", .mode = CS_MODE_MOS65XX_65816_LONG_M },
-	{ .str = "CS_MODE_MOS65XX_65816_LONG_X", .mode = CS_MODE_MOS65XX_65816_LONG_X },
-	{ .str = "CS_MODE_MOS65XX_65816_LONG_MX", .mode =  CS_MODE_MOS65XX_65816_LONG_M | CS_MODE_MOS65XX_65816_LONG_X },
+	{ .str = "CS_MODE_MOS65XX_65816_LONG_M",
+	  .mode = CS_MODE_MOS65XX_65816_LONG_M },
+	{ .str = "CS_MODE_MOS65XX_65816_LONG_X",
+	  .mode = CS_MODE_MOS65XX_65816_LONG_X },
+	{ .str = "CS_MODE_MOS65XX_65816_LONG_MX",
+	  .mode = CS_MODE_MOS65XX_65816_LONG_M | CS_MODE_MOS65XX_65816_LONG_X },
 	{ .str = "CS_MODE_SH2", .mode = CS_MODE_SH2 },
 	{ .str = "CS_MODE_SH2A", .mode = CS_MODE_SH2A },
 	{ .str = "CS_MODE_SH3", .mode = CS_MODE_SH3 },
