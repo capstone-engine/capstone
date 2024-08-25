6a0b2cbbeca44f6a160d058da50c2d82b62b3574
Rot127 <unisono@quyllur.org>
2024-08-25 05:18:33 -0500

    Remove and partially move legacy tests.
    - Not removed are the legacy integration tests.
    - Detail and asm tests are now in the yaml test files.

--- bindings/python/tests/test_all.py
+++ bindings/python/tests/test_all.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python3
 
-import test_basic, test_arm, test_aarch64, test_detail, test_lite, test_m68k, test_mips, \
-    test_ppc, test_x86, test_skipdata, test_sparc, test_systemz, test_tms320c64x, test_customized_mnem, \
-    test_m680x, test_mos65xx, test_xcore, test_riscv, test_alpha, test_hppa
+import test_lite
+import test_skipdata
+import test_customized_mnem
 
-test_basic.test_class()
-test_arm.test_class()
-test_aarch64.test_class()
-test_detail.test_class()
 test_lite.test_class()
-test_m68k.test_class()
-test_mips.test_class()
-test_mos65xx.test_class()
-test_ppc.test_class()
-test_sparc.test_class()
-test_systemz.test_class()
-test_x86.test_class()
-test_tms320c64x.test_class()
-test_m680x.test_class()
 test_skipdata.test_class()
 test_customized_mnem.test()
-test_xcore.test_class()
-test_riscv.test_class()
-test_alpha.test_class()
-test_hppa.test_class()
