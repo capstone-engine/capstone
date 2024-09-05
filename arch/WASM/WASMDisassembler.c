ecbea1586b0d6fc62cacdda7d301c70f7e5115d1
Rot127 <unisono@quyllur.org>
2024-09-05 08:02:10 -0500

    Remove dead code

--- arch/WASM/WASMDisassembler.c
+++ arch/WASM/WASMDisassembler.c
@@ -459,9 +459,6 @@ static bool read_memoryimmediate(const uint8_t *code, size_t code_len, uint16_t 
 
 	len = tmp;
 	data[1] = get_varuint32(&code[len], code_len - len, &tmp);
-	if (len == -1) {
-		return false;
-	}
 
 	if (MI->flat_insn->detail) {
 		MI->flat_insn->detail->wasm.operands[1].type = WASM_OP_VARUINT32;
