08e57cb347edc7e7634c6bd02d8bd96ede1a8c58
Rot127 <unisono@quyllur.org>
2025-05-20 07:00:24 -0500

    Update sorting code which still segfaults occasionally.

--- suite/auto-sync/src/autosync/cpptranslator/tree_sitter_compatibility.py
+++ suite/auto-sync/src/autosync/cpptranslator/tree_sitter_compatibility.py
@@ -7,9 +7,14 @@ from tree_sitter import Node, Query
 def query_captures_22_3(query: Query, node: Node) -> list[tuple[Node, str]]:
     result = list()
     captures = query.captures(node)
-    while len(captures) != 0:
-        for name, nodes in captures.items():
+    # Captures are no longer sorted by start point.
+    captures_sorted = dict()
+    nodes: list[Node]
+    for name, nodes in captures.items():
+        captures_sorted[name] = sorted(nodes, key=lambda n: n.start_point)
+    while len(captures_sorted) != 0:
+        for name, nodes in captures_sorted.items():
             node = nodes.pop(0)
             result.append((node, name))
-        captures = {k: l for k, l in captures.items() if len(l) != 0}
+        captures_sorted = {k: l for k, l in captures_sorted.items() if len(l) != 0}
     return result
