5fbad758c017d1a34d1ce46dfdc1141e3532e422
Rot127 <unisono@quyllur.org>
2025-06-05 07:44:53 -0500

    Print immediate only memory operands for AArch64.

--- cstool/cstool_aarch64.c
+++ cstool/cstool_aarch64.c
@@ -53,7 +53,7 @@ void print_insn_detail_aarch64(csh handle, cs_insn *ins)
 				printf("\t\t\toperands[%u].mem.base: REG = %s\n", i, cs_reg_name(handle, op->mem.base));
 			if (op->mem.index != AARCH64_REG_INVALID)
 				printf("\t\t\toperands[%u].mem.index: REG = %s\n", i, cs_reg_name(handle, op->mem.index));
-			if (op->mem.disp != 0)
+			if (op->mem.disp != 0 || op->mem.base == AARCH64_REG_INVALID)
 				printf("\t\t\toperands[%u].mem.disp: 0x%x\n", i, op->mem.disp);
 			if (ins->detail->aarch64.post_index)
 				printf("\t\t\tpost-indexed: true\n");
