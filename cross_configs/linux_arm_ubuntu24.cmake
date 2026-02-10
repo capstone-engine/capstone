c7c5f1116cc034384245d9e5bdbcbd1a22bedf91
Rot127 <unisono@quyllur.org>
2026-02-10 01:00:54 +0100

    Install missing libc for ARM

--- cross_configs/linux_arm_ubuntu24.cmake
+++ cross_configs/linux_arm_ubuntu24.cmake
@@ -1,5 +1,5 @@
 # This example file is for builds on Ubunutu 24.04.
-# sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf binutils-arm-linux-gnueabihf libc6-dev-armhf-cross qemu-user-static
+# sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf binutils-arm-linux-gnueabihf libc6-armhf-cross libc6-dev-armhf-cross qemu-user-static
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
@@ -7,11 +7,11 @@ set(CMAKE_C_COMPILER   arm-linux-gnueabihf-gcc)
 set(CMAKE_ASM_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CROSS_COMPILING 1)
 
-set(CMAKE_SYSROOT /usr/arm-linux-gnueabihf)
+set(CMAKE_SYSROOT /usr/arm-linux-gnueabihf/usr)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_CROSSCOMPILING_EMULATOR qemu-arm-static;-L;${CMAKE_SYSROOT})
+set(CMAKE_CROSSCOMPILING_EMULATOR qemu-arm-static;-L;/usr/arm-linux-gnueabihf)
 
