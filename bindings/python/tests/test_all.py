ce9ff927927b2c921cbc3956867786eaa0b4a7af
مصطفي محمود كمال الدين <48567303+moste00@users.noreply.github.com>
2026-04-14 20:36:38 +0200

    add tests

--- bindings/python/tests/test_all.py
+++ bindings/python/tests/test_all.py
@@ -5,6 +5,7 @@ import test_iter
 import test_skipdata
 import test_customized_mnem
 import test_compatibility_layer
+import test_riscv_sysreg
 
 errors = []
 errors.extend(test_lite.test_class())
@@ -12,6 +13,7 @@ errors.extend(test_iter.test_class())
 errors.extend(test_skipdata.test_class())
 errors.extend(test_customized_mnem.test())
 errors.extend(test_compatibility_layer.test_compatibility())
+errors.extend(test_riscv_sysreg.test())
 
 if errors:
     print("Some errors happened. Please check the output")
