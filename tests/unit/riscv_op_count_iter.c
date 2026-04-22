8463b5f1aef138c1ecd301dc82ff273d97ba05d1
Rot127 <unisono@quyllur.org>
2026-04-22 10:31:19 +0200

    Formatting

--- tests/unit/riscv_op_count_iter.c
+++ tests/unit/riscv_op_count_iter.c
@@ -16,7 +16,8 @@ static bool test_riscv_op_count_no_stale()
 	static const int32_t expected_op_counts[] = { 2, 3, 2 };
 
 	csh handle;
-	if (cs_open(CS_ARCH_RISCV, CS_MODE_RISCV64 | CS_MODE_RISCV_C, &handle) != CS_ERR_OK) {
+	if (cs_open(CS_ARCH_RISCV, CS_MODE_RISCV64 | CS_MODE_RISCV_C,
+		    &handle) != CS_ERR_OK) {
 		return false;
 	}
 	cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON | CS_OPT_DETAIL_REAL);
