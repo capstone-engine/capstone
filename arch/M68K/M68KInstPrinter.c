7441aee2280516042561035ebca99a600c66d57d
jmestwa-coder <jmestwa@gmail.com>
2026-06-15 17:18:32 +0530

    m68k: fix off-by-one bounds check in M68K_insn_name (#2964)

--- arch/M68K/M68KInstPrinter.c
+++ arch/M68K/M68KInstPrinter.c
@@ -582,7 +582,7 @@ const char *M68K_insn_name(csh handle, unsigned int id)
 #ifdef CAPSTONE_DIET
 	return NULL;
 #else
-	if (id > ARR_SIZE(s_instruction_names)) {
+	if (id >= ARR_SIZE(s_instruction_names)) {
 		return NULL;
 	}
 	return s_instruction_names[id];
