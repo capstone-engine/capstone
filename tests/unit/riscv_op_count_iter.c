bbefdfd6e7e1b3718ba0b7529a9728b618b336d1
مصطفي محمود كمال الدين <48567303+moste00@users.noreply.github.com>
2026-08-08 20:15:27 +0300

    increase test coverage

--- tests/unit/riscv_op_count_iter.c
+++ tests/unit/riscv_op_count_iter.c
@@ -21,9 +21,7 @@ static bool test_riscv_op_count_no_stale()
 		return false;
 	}
 	cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON | CS_OPT_DETAIL_REAL);
-	cs_option(handle, CS_OPT_SYNTAX,
-		  CS_OPT_SYNTAX_REAL |
-			  CS_OPT_SYNTAX_UNCOMPRESSED_REAL);
+	cs_option(handle, CS_OPT_SYNTAX, CS_OPT_SYNTAX_REAL);
 	cs_insn *insn = cs_malloc(handle);
 
 	const uint8_t *start = code;
